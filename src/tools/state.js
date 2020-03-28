0c1c14b50d127bc1c5976b4dc4e516209b8e61e6
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-28 21:35:18 +0100

    Update state.js

--- src/tools/state.js
+++ src/tools/state.js
@@ -1,92 +1,127 @@
-export default {
-    clone: {
-        name: 'Clone',
-        description: 'Clones selected shape or group',
-        icon: { group: 'content', name: 'content_copy', color: 'rgb(191, 34, 243)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.clone',
-        type: 'clone'
+import { activeTool } from './computed/activeTools';
+
+export const state = {
+  activeTools: [],
+  activeTool,
+  clone: {
+    name: 'Clone',
+    description: 'Clones selected shape or group',
+    icon: {
+      group: 'content',
+      name: 'content_copy',
+      color: 'rgb(191, 34, 243)',
+      size: 24
     },
-    circle: {
-        name: 'Circle',
-        description: 'Draws ellipse or circle',
-        icon: { group: 'image', name: 'panorama_fish_eye', color: 'rgb(144, 254, 214)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.circle',
-        type: 'circle'
+    command: 'tools.clone',
+    type: 'clone'
+  },
+  circle: {
+    name: 'Circle',
+    description: 'Draws ellipse or circle',
+    icon: {
+      group: 'image',
+      name: 'panorama_fish_eye',
+      color: 'rgb(144, 254, 214)',
+      size: 24
     },
-    image: {
-        name: 'Image',
-        description: 'Inserts image',
-        icon: { group: 'image', name: 'image', color: 'rgb(234, 2, 130)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.image',
-        type: 'image'
+    command: 'tools.circle',
+    type: 'circle'
+  },
+  image: {
+    name: 'Image',
+    description: 'Inserts image',
+    icon: {
+      group: 'image',
+      name: 'image',
+      color: 'rgb(234, 2, 130)',
+      size: 24
     },
-    line: {
-        name: 'Line',
-        description: 'Draws a line',
-        icon: { group: 'action', name: 'timeline', color: 'rgb(95, 216, 240)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tool.line',
-        type: 'line'
+    command: 'tools.image',
+    type: 'image'
+  },
+  line: {
+    name: 'Line',
+    description: 'Draws a line',
+    icon: {
+      group: 'action',
+      name: 'timeline',
+      color: 'rgb(95, 216, 240)',
+      size: 24
     },
-    move: {
-        name: 'Move',
-        description: 'Moves selected shape or group',
-        icon: { group: 'action', name: 'visibility', color: 'rgba(255,255,255)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.move',
-        type: 'move'
+    command: 'tool.line',
+    type: 'line'
+  },
+  move: {
+    name: 'Move',
+    description: 'Moves selected shape or group',
+    icon: {
+      group: 'action',
+      name: 'visibility',
+      color: 'rgba(255,255,255)',
+      size: 24
     },
-    pen: {
-        name: 'Pen',
-        description: 'Draws a curve line',
-        icon: { group: 'content', name: 'create', color: 'rgba(255,255,255)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.pen',
-        type: 'pen'
+    command: 'tools.move',
+    type: 'move'
+  },
+  pen: {
+    name: 'Pen',
+    description: 'Draws a curve line',
+    icon: {
+      group: 'content',
+      name: 'create',
+      color: 'rgba(255,255,255)',
+      size: 24
     },
-    rect: {
-        name: 'Rectangle',
-        description: 'Draws a rectangle or square',
-        icon: { group: 'image', name: 'crop_square', color: 'rgba(255,255,255)', size: 24 },
-        active: true,
-        visible: true,
-        command: 'tools.rect',
-        type: 'rect'
+    command: 'tools.pen',
+    type: 'pen'
+  },
+  rect: {
+    name: 'Rectangle',
+    description: 'Draws a rectangle or square',
+    icon: {
+      group: 'image',
+      name: 'crop_square',
+      color: 'rgba(255,255,255)',
+      size: 24
     },
-    select: {
-        name: 'Select',
-        description: 'Select a shape or group',
-        icon: { group: 'action', name: 'pan_tool', color: 'rgba(255,255,255)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.select',
-        type: 'select'
+    active: true,
+    command: 'tools.rect',
+    type: 'rect'
+  },
+  select: {
+    name: 'Select',
+    description: 'Select a shape or group',
+    icon: {
+      group: 'action',
+      name: 'pan_tool',
+      color: 'rgba(255,255,255)',
+      size: 24
     },
-    text: {
-        name: 'Text',
-        description: 'Types a text',
-        icon: { group: 'editor', name: 'title', color: 'rgba(255,255,255)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.text',
-        type: 'text'
+    command: 'tools.select',
+    type: 'select'
+  },
+  text: {
+    name: 'Text',
+    description: 'Types a text',
+    icon: {
+      group: 'editor',
+      name: 'title',
+      color: 'rgba(255,255,255)',
+      size: 24
     },
-    zoom: {
-        name: 'Zoom',
-        description: 'Zooms in and out',
-        icon: { group: 'action', name: 'search', color: 'rgba(255,255,255)', size: 24 },
-        active: false,
-        visible: true,
-        command: 'tools.zoom',
-        type: 'zoom'
-    }
-}
+    command: 'tools.text',
+    type: 'text'
+  },
+  zoom: {
+    name: 'Zoom',
+    description: 'Zooms in and out',
+    icon: {
+      group: 'action',
+      name: 'search',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    command: 'tools.zoom',
+    type: 'zoom'
+  }
+};
