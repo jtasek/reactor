d70877609a9134b4ce2e0e536b46a81a086b3fa6
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-28 21:33:40 +0100

    Update index

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,34 +1,17 @@
+import { state } from './state';
+import * as actions from './actions';
 
-import lineAdded from './signals/lineAdded'
-import toolActivated from './signals/toolActivated'
-import state from './state'
+import { Circle } from './components/Circle';
+import { Clone } from './components/Clone';
+import { Image } from './components/Image';
+import { Line } from './components/Line';
+import { Move } from './components/Move';
+import { Pen } from './components/Pen';
+import { Rectangle } from './components/Rectangle';
+import { Select } from './components/Select';
+import { Text } from './components/Text';
+import { Zoom } from './components/Zoom';
 
-import { Circle } from './components/Circle'
-import { Clone } from './components/Clone'
-import { Image } from './components/Image'
-import { Line } from './components/Line'
-import { Move } from './components/Move'
-import { Pen } from './components/Pen'
-import { Rectangle } from './components/Rectangle'
-import { Select } from './components/Select'
-import { Text } from './components/Text'
-import { Zoom } from './components/Zoom'
+export { Circle, Clone, Image, Line, Move, Pen, Rectangle, Select, Text, Zoom };
 
-export { Circle, Clone, Image, Line, Move, Pen, Rectangle, Select, Text, Zoom }
-
-export default {
-  state: state,
-
-  // Add signals
-  signals: {
-    toolActivated,
-    lineAdded
-  },
-
-  // Add services
-  providers: {
-    hello() {
-      console.log('hello from tool service')
-    }
-  }
-}
+export { state, actions };
--- src/tools/inlineStyles.js
+++ src/tools/inlineStyles.js
@@ -2,4 +2,4 @@ export default {
     fill: 'none',
     stroke: 'rgba(71, 74, 81, 1.0)',
     strokeWidth: '2'
-}
+};
