76c58ad230bc24468ab3063ac2c2d0da3071455c
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-28 19:29:14 +0100

    Convert factories to typescript with tests

---
+++ src/app/__tests__/factories.test.ts
@@ -0,0 +1,194 @@
+import 'regenerator-runtime/runtime';
+import {
+  createApplication,
+  createCommand,
+  createDocument,
+  createGroup,
+  createLayer,
+  createLink,
+  createRuler,
+  createShape,
+  getAnonymousUser,
+  getCurrentUserName,
+  getDefaultType,
+  newApplicationName,
+  newDocumentName,
+  newGroupName,
+  newLayerName,
+  newLinkName,
+  newRulerName,
+  newShapeName,
+  sequence
+} from '../factories';
+
+import { Application } from '../types';
+import { DESTRUCTION } from 'dns';
+
+describe('factories', () => {
+  describe('sequence()', () => {
+    const generator = sequence();
+
+    it('returns numbers incrementaly', () => {
+      const actual = [
+        generator.next().value,
+        generator.next().value,
+        generator.next().value
+      ];
+
+      expect(actual).toEqual([1, 2, 3]);
+    });
+  });
+
+  describe('newApplicationName()', () => {
+    it('returns new application instance name', () => {
+      const actual = newApplicationName();
+
+      expect(actual).toContain('reactor-');
+    });
+  });
+
+  describe('newDocumentName()', () => {
+    it('returns new document name', () => {
+      const actual = newDocumentName();
+
+      expect(actual).toBe('Document_1');
+    });
+  });
+
+  describe('newGroupName()', () => {
+    it('returns new group name', () => {
+      const actual = newGroupName();
+
+      expect(actual).toBe('Group_1');
+    });
+  });
+
+  describe('newLayerName()', () => {
+    it('returns new layer name', () => {
+      const actual = newLayerName();
+
+      expect(actual).toBe('Layer_1');
+    });
+  });
+
+  describe('newLinkName()', () => {
+    it('returns new link name', () => {
+      const actual = newLinkName();
+
+      expect(actual).toBe('Link_1');
+    });
+  });
+
+  describe('newRulerName()', () => {
+    it('returns new ruler name', () => {
+      const actual = newRulerName();
+
+      expect(actual).toBe('Ruler_1');
+    });
+  });
+
+  describe('newShapeName()', () => {
+    it('returns new shape name', () => {
+      const actual = newShapeName();
+
+      expect(actual).toBe('Shape_1');
+    });
+  });
+
+  describe('getAnonymousUser()', () => {
+    it('returns user object with default values', () => {
+      const actual = getAnonymousUser();
+
+      expect(actual).toEqual(
+        expect.objectContaining({
+          id: '1',
+          isAuthenticated: true,
+          name: 'Anonymous'
+        })
+      );
+    });
+  });
+
+  describe('getCurrentUserName()', () => {
+    it('returns current user name', () => {
+      const actual = getCurrentUserName();
+
+      expect(actual).toBe('anonymous');
+    });
+  });
+
+  describe('getDefaultType()', () => {
+    it('returns default type', () => {
+      const actual = getDefaultType();
+
+      expect(actual).toBe('rect');
+    });
+  });
+
+  describe('createCommand()', () => {
+    it('creates new command object with default values', () => {
+      const actual = createCommand({ name: 'COMMAND_NAME' });
+
+      expect(actual).toHaveProperty('name', 'COMMAND_NAME');
+    });
+  });
+
+  describe('createShape()', () => {
+    it('creates new shape object with default values', () => {
+      const actual = createShape({ name: 'SHAPE_NAME' });
+
+      expect(actual).toHaveProperty('name', 'SHAPE_NAME');
+    });
+  });
+
+  describe('createLink()', () => {
+    it('creates new link object with default values', () => {
+      const actual = createLink({ name: 'LINK_NAME' });
+
+      expect(actual).toHaveProperty('name', 'LINK_NAME');
+    });
+  });
+
+  describe('createRuler()', () => {
+    it('creates new ruler object with default values', () => {
+      const actual = createRuler({ name: 'RULER_NAME' });
+
+      expect(actual).toHaveProperty('name', 'RULER_NAME');
+    });
+  });
+
+  describe('createGroup()', () => {
+    it('creates new group object with default values', () => {
+      const actual = createGroup({ name: 'GROUP_NAME' });
+
+      expect(actual).toHaveProperty('name', 'GROUP_NAME');
+    });
+  });
+
+  describe('createLayer()', () => {
+    it('creates new layer object with default values', () => {
+      const actual = createLayer({ name: 'LAYER_NAME' });
+
+      expect(actual).toHaveProperty('name', 'LAYER_NAME');
+    });
+  });
+
+  describe('createDocument()', () => {
+    it('creates new document object with default values', () => {
+      const actual = createDocument({ name: 'DOCUMENT_NAME' });
+
+      expect(actual).toHaveProperty('name', 'DOCUMENT_NAME');
+    });
+  });
+
+  describe('createApplication()', () => {
+    it('creates new application object with default values', () => {
+      Date.now = jest.fn(() => 1);
+      const actual = createApplication();
+
+      expect(actual).toHaveProperty('id', 'reactor-1');
+
+      (Date.now as jest.Mock).mockRestore();
+    });
+  });
+});
--- src/app/factories.ts
+++ src/app/factories.ts
@@ -1,92 +1,160 @@
+import { v4 as newId } from 'uuid';
+import {
+    Application,
+    Command,
+    Document,
+    Group,
+    Layer,
+    Link,
+    Orientation,
+    Ruler,
+    Shape,
+    User
+} from './types';
 
-import {v4} from 'uuid'
-import type {Command, Shape, Link, Ruler, Group, Layer, Workspace, Application} from './types'
+export const getDefaultName = (type: string) => 'Rect';
 
-export function createCommand(options: Object): Command {
-    return Object.assign({
-        id: v4()
-    }, options)
+const documentSequence = sequence();
+const groupSequence = sequence();
+const layerSequence = sequence();
+const linkSequence = sequence();
+const rulerSequence = sequence();
+const shapeSequence = sequence();
+
+export const newApplicationName = () => `reactor-${Date.now()}`;
+export const newDocumentName = () =>
+    `Document_${documentSequence.next().value}`;
+export const newGroupName = () => `Group_${groupSequence.next().value}`;
+export const newLayerName = () => `Layer_${layerSequence.next().value}`;
+export const newLinkName = () => `Link_${linkSequence.next().value}`;
+export const newRulerName = () => `Ruler_${rulerSequence.next().value}`;
+export const newShapeName = () => `Shape_${shapeSequence.next().value}`;
+
+export function* sequence() {
+    let i = 1;
+    while (true) {
+        yield i++;
+    }
 }
 
-export function createShape(options: Object): Shape {
-    return Object.assign({
-        id: v4(),
-        locked: false,
-        visible: true,
-        selected: true,
+export const getAnonymousUser = (): User => {
+    return {
+        id: '1',
+        isAuthenticated: true,
+        lastLoggedIn: new Date(),
+        loggedIn: new Date(),
+        name: 'Anonymous'
+    };
+};
+
+export function getCurrentUserName() {
+    return 'anonymous';
+}
+
+export function getDefaultType() {
+    return 'rect';
+}
+
+export function createCommand(options: Partial<Command> = {}): Command {
+    return {
+        id: newId(),
+        category: 'test',
+        name: 'Print document name',
+        action: document => console.log(document.name),
+        ...options
+    };
+}
+
+export function createShape(options: Partial<Shape> = {}): Shape {
+    const id = newId();
+    const type = getDefaultType();
+
+    return {
+        id,
         children: [],
-        created: Date.now(),
-        createdBy: 'tasek',
-        modified: Date.now(),
-        modifiedBy: 'tasek',
-        type: 'undefined'
-    }, options)
-}
-
-export function createLink(options: Object): Link {
-    return Object.assign({
-        type: 'refer'
-    }, options)
-}
-
-export function createRuler(options: Object): Ruler {
-    return Object.assign({
-        type: 'refer'
-    }, options)
-}
-
-export function createGroup(options: Object): Group {
-    return Object.assign({
+        created: new Date(),
+        createdBy: getCurrentUserName(),
         locked: false,
-        selected: false,
-        visible: false,
-        name: 'GROUP',
-        children: []
-    }, options)
+        modified: new Date(),
+        modifiedBy: getCurrentUserName(),
+        name: newShapeName(),
+        selected: true,
+        type: getDefaultType(),
+        visible: true,
+        ...options
+    };
 }
 
-export function createLayer(options: Object): Layer {
-    return Object.assign({
+export function createLink(options: Partial<Link> = {}): Link {
+    return {
+        id: newId(),
+        name: newLinkName(),
+        ...options
+    };
+}
+
+export function createRuler(options: Partial<Ruler> = {}): Ruler {
+    return {
+        id: newId(),
+        name: newRulerName(),
+        orientation: Orientation.horizontal,
+        position: { x: 0, y: 0 },
+        ...options
+    };
+}
+
+export function createGroup(options: Partial<Group> = {}): Group {
+    return {
+        id: newId(),
         locked: false,
+        name: newGroupName(),
         selected: false,
+        shapes: [],
         visible: false,
-        name: 'LAYER',
-        children: []
-    }, options)
+        ...options
+    };
 }
 
-export function createWorkspace(options: Object): Workspace {
-    return Object.assign({
-        author: 'Jakub Tasek',
-        id: 1,
-        code: 'workspace-1',
-        name: 'Workspace 1',
-        description: 'This is my first workspace',
-        created: Date.now(),
-        createdBy: 'tasek',
-        modified: Date.now(),
-        modifiedBy: 'tasek',
-        filter: '',
-        grid: {
-            width: 10,
-            visible: true,
-            factor: 10,
-            height: 10
-        },
-        groups: [],
-        layers: [],
-        links: [],
-        rulers: [],
-        shapes: []
-    }, options)
+export function createLayer(options: Partial<Layer> = {}): Layer {
+    return {
+        id: newId(),
+        locked: false,
+        name: newLayerName(),
+        selected: false,
+        shapes: [],
+        visible: false,
+        ...options
+    };
 }
 
-export function createApplication(options: Object): Application {
-    return Object.assign({
-        commands: [],
-        config: {},
-        name: 'reactor',
-        stats: {},
-        workspaces: []
-    }, options)
+export function createDocument(options: Partial<Document> = {}): Document {
+    return {
+        id: newId(),
+        author: getCurrentUserName(),
+        created: new Date(),
+        description: '',
+        grid: { width: 10, visible: true, factor: 10, height: 10 },
+        groups: {},
+        history: [],
+        layers: {},
+        links: {},
+        modified: new Date(),
+        name: newDocumentName(),
+        rulers: {},
+        scale: 1,
+        selection: [],
+        shapes: {},
+        ...options
+    };
+}
+
+export function createApplication(
+    options: Partial<Application> = {}
+): Partial<Application> {
+    return {
+        id: newApplicationName(),
+        started: new Date(),
+        user: getAnonymousUser(),
+        ...options
+    };
 }
