2fa44de74fc0c8b6fcefa8ab74a1bba1f4b221c6
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-29 00:23:02 +0100

    Rename group actions

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,32 +1,38 @@
-import { addGroup } from './addGroup';
+export {
+  addGroup,
+  cloneGroup,
+  hideGroup,
+  lockGroup,
+  removeGroup,
+  selectGroup,
+  showGroup,
+  unlockGroup,
+  unselectGroup,
+  updateGroup
+} from './group';
 import { addLayer } from './addLayer';
 import { addLink } from './addLink';
 import { addRuler } from './addRuler';
 import { addShape } from './addShape';
 import { addWorkspace } from './addWorkspace';
 import { clearWorkspace } from './clearWorkspace';
-import { cloneGroup } from './cloneGroup';
 import { cloneLayer } from './cloneLayer';
 import { cloneLink } from './cloneLink';
 import { cloneRuler } from './cloneRuler';
 import { cloneShape } from './cloneShape';
 import { cloneWorkspace } from './cloneWorkspace';
 import { executeCommand } from './executeCommand';
-import { removeGroup } from './removeGroup';
 import { removeLayer } from './removeLayer';
 import { removeLink } from './removeLink';
 import { removeRuler } from './removeRuler';
 import { removeShape } from './removeShape';
 import { removeWorkspace } from './removeWorkspace';
 import { search } from './search';
-import { selectGroup } from './selectGroup';
 import { selectLayer } from './selectLayer';
 import { selectLink } from './selectLink';
 import { selectRuler } from './selectRuler';
 import { selectShape } from './selectShape';
 import { selectWorkspace } from './selectWorkspace';
-import { toggleGroupIsLocked } from './toggleGroupIsLocked';
-import { toggleGroupIsVisible } from './toggleGroupIsVisible';
 import { toggleLayerIsLocked } from './toggleLayerIsLocked';
 import { toggleLayerIsVisible } from './toggleLayerIsVisible';
 import { toggleLinkIsLocked } from './toggleLinkIsLocked';
@@ -35,7 +41,6 @@ import { toggleRulerIsLocked } from './toggleRulerIsLocked';
 import { toggleRulerIsVisible } from './toggleRulerIsVisible';
 import { toggleShapeIsLocked } from './toggleShapeIsLocked';
 import { toggleShapeIsVisible } from './toggleShapeIsVisible';
-import { updateGroup } from './updateGroup';
 import { updateLayer } from './updateLayer';
 import { updateLink } from './updateLink';
 import { updateRuler } from './updateRuler';
@@ -43,35 +48,29 @@ import { updateShape } from './updateShape';
 import { updateWorkspace } from './updateWorkspace';
 
 export {
-  addGroup,
   addLayer,
   addLink,
   addRuler,
   addShape,
   addWorkspace,
   clearWorkspace,
-  cloneGroup,
   cloneLayer,
   cloneLink,
   cloneRuler,
   cloneShape,
   cloneWorkspace,
   executeCommand,
-  removeGroup,
   removeLayer,
   removeLink,
   removeRuler,
   removeShape,
   removeWorkspace,
   search,
-  selectGroup,
   selectLayer,
   selectLink,
   selectRuler,
   selectShape,
   selectWorkspace,
-  toggleGroupIsLocked,
-  toggleGroupIsVisible,
   toggleLayerIsLocked,
   toggleLayerIsVisible,
   toggleLinkIsLocked,
@@ -80,7 +79,6 @@ export {
   toggleRulerIsVisible,
   toggleShapeIsLocked,
   toggleShapeIsVisible,
-  updateGroup,
   updateLayer,
   updateLink,
   updateRuler,
