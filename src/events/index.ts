75083d3dd5609e1935c97fdc1e470dfeb41c2eac
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-28 20:59:49 +0100

    Update index.ts

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,19 +1,4 @@
+import { state } from './state';
+import * as actions from './actions';
 
-import dragging from './signals/dragging'
-import endDragging from './signals/endDragging'
-import startDragging from './signals/startDragging'
-import state from './state'
-
-export default Module({
-  signals: {
-    startDragging,
-    dragging,
-    endDragging
-  },
-  state: state,
-  providers: {
-    hello() {
-      console.log('hello from service')
-    }
-  }
-})
+export { state, actions };
