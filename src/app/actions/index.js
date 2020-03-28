bf925bae398c559b4f11125df024a7f74d98c077
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-28 18:40:06 +0100

    Add index file to actions

---
+++ src/app/actions/index.js
@@ -0,0 +1,89 @@
+import { addGroup } from './addGroup';
+import { addLayer } from './addLayer';
+import { addLink } from './addLink';
+import { addRuler } from './addRuler';
+import { addShape } from './addShape';
+import { addWorkspace } from './addWorkspace';
+import { clearWorkspace } from './clearWorkspace';
+import { cloneGroup } from './cloneGroup';
+import { cloneLayer } from './cloneLayer';
+import { cloneLink } from './cloneLink';
+import { cloneRuler } from './cloneRuler';
+import { cloneShape } from './cloneShape';
+import { cloneWorkspace } from './cloneWorkspace';
+import { executeCommand } from './executeCommand';
+import { removeGroup } from './removeGroup';
+import { removeLayer } from './removeLayer';
+import { removeLink } from './removeLink';
+import { removeRuler } from './removeRuler';
+import { removeShape } from './removeShape';
+import { removeWorkspace } from './removeWorkspace';
+import { search } from './search';
+import { selectGroup } from './selectGroup';
+import { selectLayer } from './selectLayer';
+import { selectLink } from './selectLink';
+import { selectRuler } from './selectRuler';
+import { selectShape } from './selectShape';
+import { selectWorkspace } from './selectWorkspace';
+import { toggleGroupIsLocked } from './toggleGroupIsLocked';
+import { toggleGroupIsVisible } from './toggleGroupIsVisible';
+import { toggleLayerIsLocked } from './toggleLayerIsLocked';
+import { toggleLayerIsVisible } from './toggleLayerIsVisible';
+import { toggleLinkIsLocked } from './toggleLinkIsLocked';
+import { toggleLinkIsVisible } from './toggleLinkIsVisible';
+import { toggleRulerIsLocked } from './toggleRulerIsLocked';
+import { toggleRulerIsVisible } from './toggleRulerIsVisible';
+import { toggleShapeIsLocked } from './toggleShapeIsLocked';
+import { toggleShapeIsVisible } from './toggleShapeIsVisible';
+import { updateGroup } from './updateGroup';
+import { updateLayer } from './updateLayer';
+import { updateLink } from './updateLink';
+import { updateRuler } from './updateRuler';
+import { updateShape } from './updateShape';
+import { updateWorkspace } from './updateWorkspace';
+
+export {
+  addGroup,
+  addLayer,
+  addLink,
+  addRuler,
+  addShape,
+  addWorkspace,
+  clearWorkspace,
+  cloneGroup,
+  cloneLayer,
+  cloneLink,
+  cloneRuler,
+  cloneShape,
+  cloneWorkspace,
+  executeCommand,
+  removeGroup,
+  removeLayer,
+  removeLink,
+  removeRuler,
+  removeShape,
+  removeWorkspace,
+  search,
+  selectGroup,
+  selectLayer,
+  selectLink,
+  selectRuler,
+  selectShape,
+  selectWorkspace,
+  toggleGroupIsLocked,
+  toggleGroupIsVisible,
+  toggleLayerIsLocked,
+  toggleLayerIsVisible,
+  toggleLinkIsLocked,
+  toggleLinkIsVisible,
+  toggleRulerIsLocked,
+  toggleRulerIsVisible,
+  toggleShapeIsLocked,
+  toggleShapeIsVisible,
+  updateGroup,
+  updateLayer,
+  updateLink,
+  updateRuler,
+  updateShape,
+  updateWorkspace
+};
