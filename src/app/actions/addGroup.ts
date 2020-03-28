f0a0a2bb71e68fc5d41931361d76dd12dc698f44
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-29 00:14:17 +0100

    Consolidate group actions

--- src/app/actions/addGroup.ts
+++ src/app/actions/addGroup.ts
@@ -1,9 +1,45 @@
-import {createGroup} from '../app/factories'
+import { Application } from '../types';
+import { createGroup } from '../factories';
 
-export default ({props, state}) => {
-    const group = createGroup(props)
+export const addGroup = ({ currentDocument: { state } }, props) => {
+  const group = createGroup(props);
 
-    state.push(`workspace.groups.${group.id}`, group)
+  const { currentDocument } = state;
+  state.push(`workspace.groups.${group.id}`, group);
+};
 
-    output({ id: group.id })
-}
+export const cloneGroup = ({ props, state }) => {
+  const group = state.get(`workspace.groups.${props.id}`);
+  group.id = v4();
+
+  state.push(`workspace.groups.${group.id}`, group);
+
+  output({ id: group.id });
+};
+
+export const removeGroup = ({ props, state }) => {
+  state.unset(`workspace.groups.${props.id}`);
+};
+
+export const selectGroup = ({ props, state }) => {
+  state.set(`workspace.groups.${props.id}.selected`, true);
+};
+
+export const lockGroup = ({ props, state }) => {
+  const path = `workspace.groups.${props.id}.locked`;
+  state.set(path, !state.get(path));
+};
+
+export const showGroup = ({ props, state }) => {
+  const path = `workspace.groups.${props.id}.visible`;
+  state.set(path, !state.get(path));
+};
+
+export const hideGroup = ({ props, state }) => {
+  const path = `workspace.groups.${props.id}.visible`;
+  state.set(path, !state.get(path));
+};
+
+export const updateGroup = ({ props, state }) => {
+  state.merge(`workspace.layers.${props.id}`, props);
+};
--- src/app/actions/selectGroup.ts
+++
@@ -1,3 +0,0 @@
-export default ({props, state}) => {
-  state.set(`workspace.groups.${props.id}.selected`, true)
-}
