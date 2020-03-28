7e3f5597712612c23224425bc79694408c527642
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-28 21:40:12 +0100

    Update index.ts

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,29 +1,5 @@
+import * as actions from './actions';
+import { state } from './state';
+import commands from '../tools/commands';
 
-import contextMenuDisplayed from '../app/signals/contextMenuDisplayed'
-import controlVisibilityChanged from '../app/signals/controlVisibilityChanged'
-import displayStatusMessage from '../app/signals/displayStatusMessage'
-import groupVisibilityChanged from '../app/signals/groupVisibilityChanged'
-import layerVisibilityChanged from '../app/signals/layerVisibilityChanged'
-import shapeSelectionChanged from '../app/signals/shapeSelectionChanged'
-import state from './state'
-
-export default {
-  state: state,
-
-  // Add signals
-  signals: {
-    contextMenuDisplayed,
-    controlVisibilityChanged,
-    groupVisibilityChanged,
-    layerVisibilityChanged,
-    shapeSelectionChanged,
-    displayStatusMessage
-  },
-
-  // Add services
-  services: {
-    hello() {
-      console.log('hello from service')
-    }
-  }
-}
+export { actions, commands, state };
