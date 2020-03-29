821a0b7ee3d433c5e28b1b30e2290e09d5a78357
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-03-30 00:36:23 +0200

    Consolidate ruler actions file

--- src/app/actions/addRuler.ts
+++ src/app/actions/addRuler.ts
@@ -1,9 +1,67 @@
-import {createRuler} from '../app/factories'
+import { createRuler, newId } from '../factories';
 
-export default ({props, state}) => {
-    const ruler = createRuler(props)
+const getRuler = (state, rulerId) => state.currentDocument.rulers[rulerId];
 
-    state.push(`workspace.rulers.${ruler.id}`, ruler)
+const setRuler = (state, ruler) =>
+  (state.currentDocument.rulers[ruler.id] = ruler);
 
-    output({ id: ruler.id })
-}
+const deleteRuler = (state, rulerId) =>
+  delete state.currentDocument.rulers[rulerId];
+
+export const addruler = ({ state }, options) => {
+  const ruler = createRuler(options);
+
+  setRuler(state, ruler);
+};
+
+export const cloneruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  setRuler(state, { ...ruler, id: newId() });
+};
+
+export const removeruler = ({ state }, rulerId) => {
+  deleteRuler(state, rulerId);
+};
+
+export const selectruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  ruler.selected = true;
+};
+
+export const unselectruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  ruler.selected = false;
+};
+
+export const lockruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  ruler.locked = true;
+};
+
+export const unlockruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  ruler.locked = false;
+};
+
+export const showruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  ruler.visible = true;
+};
+
+export const hideruler = ({ state }, rulerId) => {
+  const ruler = getRuler(state, rulerId);
+
+  ruler.visible = false;
+};
+
+export const updateruler = ({ state }, options) => {
+  const ruler = getRuler(state, options.id);
+
+  setRuler(state, { ...ruler, options });
+};
--- src/app/actions/removeRuler.ts
+++
@@ -1,3 +0,0 @@
-export default ({props, state}) => {
-    state.unset(`workspace.rulers.${props.id}`)
-}
