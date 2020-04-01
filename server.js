e5002b362d5cc54bef0a230752199c961ccb6e44
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-01 23:28:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import path from 'path'
-import express from 'express'
-import webpack from 'webpack'
-import webpackMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-import config from './webpack.config.js'
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
 
-const app = express()
-const compiler = webpack(config)
-const { PORT = 4000 } = process.env
+const app = express();
+const compiler = webpack(config);
+const { PORT = 4000 } = process.env;
 
 app.use(
   webpackMiddleware(compiler, {
@@ -27,26 +27,26 @@ app.use(
     //   modules: false
     // }
   })
-)
+);
 
 app.use(
   webpackHotMiddleware(compiler, {
     reload: true // reload page when webpack gets stuck
   })
-)
+);
 
-app.use('/icons', express.static(__dirname + '/static/icons/'))
-app.use('/images', express.static(__dirname + '/static/images/'))
-app.use('/styles', express.static(__dirname + '/static/styles/'))
+app.use('/icons', express.static(__dirname + '/static/icons/'));
+app.use('/images', express.static(__dirname + '/static/images/'));
+app.use('/styles', express.static(__dirname + '/static/styles/'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './src/index.html'))
-})
+  res.sendFile(path.join(__dirname, './src/index.html'));
+});
 
-app.listen(PORT, 'localhost', err => {
+app.listen(PORT, 'localhost', (err) => {
   if (err) {
-    return console.log(err)
+    return console.log(err);
   }
 
-  console.log(`Listening at http://localhost:${PORT}`)
-})
+  console.log(`Listening at http://localhost:${PORT}`);
+});
