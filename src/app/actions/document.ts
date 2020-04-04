cb860e338ae1f7af730e217a2205fee9ad02124b
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-04 22:05:32 +0200

    Convert document to TS

---
+++ src/app/actions/document.ts
@@ -0,0 +1,58 @@
+import { Action } from 'overmind';
+import { Application, Document } from '../types';
+import { createDocument } from '../factories';
+
+const getDocument = ({ documents }: Application, documentId: string) => documents[documentId];
+
+const setDocument = ({ documents }: Application, document: Document) =>
+  (documents[document.id] = document);
+
+const deleteDocument = ({ documents }: Application, documentId: string) => {
+  delete documents[documentId];
+};
+
+export const addDocument: Action<Partial<Document>> = ({ state }, options) => {
+  const document = createDocument(options);
+
+  setDocument(state, document);
+};
+
+export const cloneDocument: Action<string> = ({ state, effects }, documentId) => {
+  const document = getDocument(state, documentId);
+
+  setDocument(state, { ...document, id: effects.newId() });
+};
+
+export const removeDocument: Action<string> = ({ state }, documentId) => {
+  deleteDocument(state, documentId);
+};
+
+export const selectDocument: Action<string> = ({ state }, documentId) => {
+  const document = getDocument(state, documentId);
+
+  document.selected = true;
+};
+
+export const unselectDocument: Action<string> = ({ state }, documentId) => {
+  const document = getDocument(state, documentId);
+
+  document.selected = false;
+};
+
+export const lockDocument: Action<string> = ({ state }, documentId) => {
+  const document = getDocument(state, documentId);
+
+  document.locked = true;
+};
+
+export const unlockDocument: Action<string> = ({ state }, documentId) => {
+  const document = getDocument(state, documentId);
+
+  document.locked = false;
+};
+
+export const updateDocument: Action<Partial<Document> & { id: string }> = ({ state }, options) => {
+  const document = getDocument(state, options.id);
+
+  setDocument(state, { ...document, ...options });
+};
