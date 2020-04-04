3c5f145f79fd9486fa843e501f419727c3f6450d
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-04 22:06:01 +0200

    Update localStorage.ts

--- src/app/services/localStorage.ts
+++ src/app/services/localStorage.ts
@@ -1,20 +1,25 @@
-export const loadState = (key) => {
-    try {
-        const serializedState = localStorage.getItem(key)
+import { Application } from '../types';
 
-        return JSON.parse(serializedState)
-    }
-    catch (error) {
-        console.error('Failed to load application state', error)
-    }
-}
+export const loadState = (key: string) => {
+  try {
+    const serializedState = localStorage.getItem(key);
 
-export const saveState = (key, state) => {
-    try {
-        const serializedState = JSON.stringify(state)
-
-        localStorage.setItem(key, serializedState)
-    } catch (error) {
-        console.error('Failed to save application state', error)
+    if (!serializedState) {
+      return null;
     }
-}
+
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Failed to load application state', error);
+  }
+};
+
+export const saveState = (key: string, state: Application) => {
+  try {
+    const serializedState = JSON.stringify(state);
+
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Failed to save application state', error);
+  }
+};
