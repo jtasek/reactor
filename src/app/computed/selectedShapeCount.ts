3fd538df572149bdca40e084ae97e1e70e4717b1
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-05 10:58:22 +0200

    Update selectedShapeCount.ts

--- src/app/computed/selectedShapeCount.ts
+++ src/app/computed/selectedShapeCount.ts
@@ -1,2 +1,5 @@
-export const selectedShapeCount = (selectedShapes: { currentDocument }) =>
-  selectedShapes.length;
+import { Derive } from 'overmind';
+import { Application } from '../types';
+
+export const selectedShapeCount: Derive<Application, number> = ({ currentDocument }) =>
+  currentDocument?.selectedShapes.length ?? 0;
