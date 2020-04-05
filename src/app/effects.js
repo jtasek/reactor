1f439a9856e657bdcbc7bc28abb6d5cc568ec80b
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-05 11:30:52 +0200

    Update effects.js

--- src/app/effects.js
+++ src/app/effects.js
@@ -1,3 +1,16 @@
+import page from 'page';
 export { v4 as newId } from 'uuid';
 
 export { execute } from './services/commandInterpreter';
+
+export const router = {
+  initialize(routes) {
+    Object.keys(routes).forEach((url) => {
+      page(url, ({ params }) => routes[url](params));
+    });
+    page.start();
+  },
+  goTo(url) {
+    page.show(url);
+  }
+};
