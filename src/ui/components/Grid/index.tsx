3690d58de3fec09b39719f26fa1bd7d510103baa
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-05 10:13:27 +0200

    Update index.tsx

--- src/ui/components/Grid/index.tsx
+++ src/ui/components/Grid/index.tsx
@@ -1,58 +1,62 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import styles from './styles.css';
+import { useApp } from '../../../app';
+import { Camera, Grid } from '../../../app/types';
 
-function getGridPath(width, height, scale) {
+function getGridPath(width: number, height: number, scale: number) {
   return `M ${width * scale} 0 L 0 0 0 ${height * scale}`;
 }
 
-const Grid = ({ grid, camera }) => {
+interface Props {
+  camera: Camera;
+  grid: Grid;
+}
+
+const Grid: FC<Props> = ({ grid, camera }) => {
   const smallGridHeight = grid.height * camera.scale;
   const smallGridWidth = grid.width * camera.scale;
   const smallGridPath = getGridPath(grid.width, grid.height, camera.scale);
   const gridHeight = grid.height * grid.factor * camera.scale;
   const gridWidth = grid.width * grid.factor * camera.scale;
-  const gridPath = getGridPath(
-      grid.width * grid.factor,
-      grid.height * grid.factor,
-      camera.scale
-  );
+  const gridPath = getGridPath(grid.width * grid.factor, grid.height * grid.factor, camera.scale);
   const transform = `translate(${camera.position.x}, ${camera.position.y})`;
 
   return (
-      <g
-          style={!grid.visible ? { display: 'none' } : { display: 'block' }}
-          width="100%"
-          height="100%"
-      >
-        <defs>
-          <pattern
-              id="smallGrid"
-              width={smallGridWidth}
-              height={smallGridHeight}
-              patternUnits="userSpaceOnUse"
-          >
-            <path d={smallGridPath} stroke="gray" strokeWidth="0.5" fill="none" />
-          </pattern>
-          <pattern
-              id="grid"
-              width={gridWidth}
-              height={gridHeight}
-              patternUnits="userSpaceOnUse"
-              patternTransform={transform}
-          >
-            <rect width={gridWidth} height={gridHeight} fill="url(#smallGrid)" />
-            <path d={gridPath} stroke="gray" strokeWidth="1" fill="none" />
-          </pattern>
-        </defs>
-        <rect width="100%" height="100%" fill="url(#grid)" />
-      </g>
+    <g
+      style={!grid.visible ? { display: 'none' } : { display: 'block' }}
+      width="100%"
+      height="100%"
+    >
+      <defs>
+        <pattern
+          id="smallGrid"
+          width={smallGridWidth}
+          height={smallGridHeight}
+          patternUnits="userSpaceOnUse"
+        >
+          <path d={smallGridPath} stroke="gray" strokeWidth="0.5" fill="none" />
+        </pattern>
+        <pattern
+          id="grid"
+          width={gridWidth}
+          height={gridHeight}
+          patternUnits="userSpaceOnUse"
+          patternTransform={transform}
+        >
+          <rect width={gridWidth} height={gridHeight} fill="url(#smallGrid)" />
+          <path d={gridPath} stroke="gray" strokeWidth="1" fill="none" />
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" fill="url(#grid)" />
+    </g>
   );
 };
 
-export default connect(
-    {
-      camera: state`workspace.camera`,
-      grid: state`ui.controls.grid`
-    },
-    Grid
-);
+export default () => {
+  const { state } = useApp();
+
+  const camera = state.currentDocument?.camera;
+  const grid = state.ui.controls.grid;
+
+  return <Grid camera={camera} grid={grid} />;
+};
