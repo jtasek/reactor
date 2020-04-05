363c13a96a963bc4034ffcace2cdc66a659bbb10
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-05 11:15:55 +0200

    Update actions.ts

--- src/ui/actions.ts
+++ src/ui/actions.ts
@@ -1,5 +1,26 @@
+import { Action } from 'overmind';
 import { UI } from './types';
 
-export const showControl = ({ state: UI }, control: string) => {
-  UI[control].visible = true;
+export const showControl: Action<string> = ({ controls }: UI, controlId: string) => {
+  controls[controlId].visible = true;
+};
+
+export const hideControl = ({ controls }: UI, controlId: string) => {
+  controls[controlId].visible = false;
+};
+
+export const distructionFreeMode: Action<boolean> = ({ controls }: UI) => {
+  controls.commandLine.visible = false;
+  controls.contextMenu.visible = false;
+  controls.controlPanel.visible = false;
+  controls.groupPanel.visible = false;
+  controls.layerPanel.visible = false;
+  controls.menuBar.visible = false;
+  controls.minimap.visible = false;
+  controls.navBar.visible = false;
+  controls.propertyPanel.visible = false;
+  controls.sideBar.visible = false;
+  controls.statusBar.visible = false;
+  controls.surface.visible = false;
+  controls.toolBar.visible = false;
 };
