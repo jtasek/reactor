bdb1739ccc24db0fb7d755ca35c2f98caadbe175
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-05 11:30:59 +0200

    Update onInitialize.ts

--- src/app/onInitialize.ts
+++ src/app/onInitialize.ts
@@ -1,12 +1,16 @@
 export const onInitialize = ({ state, actions, effects }, instance) => {
-  state.todos = effects.storage.getTodos();
+  // Register controls
+  // Register commands
 
+  // Load data from storage
+  state.todos = effects.storage.getTodos();
+  // Activate autosave
   instance.reaction(
     ({ todos }) => todos,
-    todos => effects.storage.saveTodos(todos),
+    (todos) => effects.storage.saveTodos(todos),
     { nested: true }
   );
-
+  // Activate routing
   effects.router.initialize({
     '/': () => actions.changeFilter('all'),
     '/active': () => actions.changeFilter('active'),
