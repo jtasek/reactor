9423eea631e038f133485aa9574d8a9f7748c012
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-04-06 08:11:29 +0200

    Update routes.ts

--- src/app/actions/routes.ts
+++ src/app/actions/routes.ts
@@ -1,15 +1,16 @@
-export default [
-  {
-    path: '/',
-    signal: 'applicationStarted'
-  }
-  // '/login': '',
-  // '/reset':
-  // '/signup': '',
-  // '/logout': '',
-  // '/terms': '',
-  // '/privacy': '',
-  // '/export': '',
-  // '/import': '',
-  // '/about': ''
-];
+import { Action } from 'overmind';
+
+export const registerRoutes: Action = ({ actions, effects }) => {
+  effects.router.initialize({
+    '/': () => actions.changeFilter('all'),
+    '/login': () => actions.login(),
+    '/reset': () => actions.reset(),
+    '/signup': () => actions.singup(),
+    '/logout': () => actions.logour(),
+    '/terms': () => actions.terms(),
+    '/privacy': () => actions.privacy(),
+    '/export': () => actions.export(),
+    '/import': () => actions.import(),
+    '/about': () => actions.about()
+  });
+};
