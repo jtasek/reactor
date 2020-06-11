6a8314b24f971ff2f57901812b9ad4e850b86ecc
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-06-11 23:31:48 +0200

    Update index.tsx

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -1,8 +1,5 @@
 import React, { FC } from 'react';
-//import {DragDropContext} from 'react-dnd'
-//import HTML5Backend from 'react-dnd-html5-backend'
 
 import { Layout } from '../../../ui/components/Layout';
 
-//export default DragDropContext(HTML5Backend)(
 export const App: FC = () => <Layout />;
