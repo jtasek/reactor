8282d5b085fa8044918bef4797c7984d0dee7b34
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-10-05 08:14:16 +0200

    Refactor app tests

--- src/app/__tests__/utils.test.ts
+++ src/app/__tests__/utils.test.ts
@@ -12,7 +12,7 @@ describe('utils', () => {
   });
 
   describe('pointInRectangle()', () => {
-    it('returns true when point is inside of rectangle ', () => {
+    it('returns true when point is inside of rectangle', () => {
       const actual = pointInRectangle(
         { x: 50, y: 50 },
         { p: { x: 1, y: 1 }, size: { width: 100, height: 100 } }
@@ -21,7 +21,7 @@ describe('utils', () => {
       expect(actual).toBe(true);
     });
 
-    it('returns false when point is outside of rectangle ', () => {
+    it('returns false when point is outside of rectangle', () => {
       const actual = pointInRectangle(
         { x: 1, y: 1 },
         { p: { x: 50, y: 50 }, size: { width: 100, height: 100 } }
