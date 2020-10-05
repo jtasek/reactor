011a7fd51f0ccf506171734eabcb000bcc5c5234
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-10-05 08:16:02 +0200

    Update onInitialize.ts

--- src/app/onInitialize.ts
+++ src/app/onInitialize.ts
@@ -1,14 +1,34 @@
-export const onInitialize = ({ state, actions, effects }, instance) => {
+import { OnInitialize } from 'overmind';
+
+export const onInitialize: OnInitialize = ({ state, actions, effects }, instance) => {
   // Register commands
 
   // Load data from storage
-  state.todos = effects.storage.getTodos();
+  const savedApp = effects.loadState('reactor');
+  if (savedApp) {
+    state.documents = savedApp.documents;
+  }
+
   // Activate autosave
   instance.reaction(
-    ({ todos }) => todos,
-    (todos) => effects.storage.saveTodos(todos),
+    (state) => state,
+    (state) => effects.saveState('reactor', state),
     { nested: true }
   );
+
   // Activate routing
-  actions.registerRouters();
+  effects.registerRouters({
+    '/': () => actions.showHomePage(),
+    '/about': () => actions.aboutPage(),
+    '/export': () => actions.exportPage(),
+    '/import': () => actions.importPage(),
+    '/login': () => actions.showLoginPage(),
+    '/logout': () => actions.showLogoutPage(),
+    '/privacy': () => actions.privacyPage(),
+    '/reset': () => actions.showResetPage(),
+    '/signup': () => actions.showSingupPage(),
+    '/terms': () => actions.showTermsPage()
+  });
 };
+
+export default onInitialize;
