25e43c4839a4b7941998cd14202833d8a6aab6ec
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-10-30 21:09:47 +0100

    Update types.ts

--- src/tools/types.ts
+++ src/tools/types.ts
@@ -1,1 +1,22 @@
-export interface Tools {}
+import { Action } from '../app/types';
+
+export interface Tool {
+  id: string;
+  active: boolean;
+  code: string;
+  description?: string;
+  handler: Action<string>;
+  icon: Icon;
+}
+
+export interface Icon {
+  color: string;
+  group: string;
+  name: string;
+  size: number;
+}
+
+export interface Tools {
+  activeToolsIds: string[];
+  activeTools: Tool[];
+}
