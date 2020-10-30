a681ce446a60d524a03ec4ca5724f12a3fd9f44e
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-10-30 20:51:59 +0100

    Update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
+const path = require('path');
+const custom = require('../webpack.config.js');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    return { ...config, module: { ...config.module, rules: custom.module.rules } };
+  }
+};
