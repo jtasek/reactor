2f048319f2c10cf4a603750ebe3cb7fc4bb785d2
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-10-30 21:03:54 +0100

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -10,9 +10,12 @@ import { merge, namespaced } from 'overmind/config';
 
 import * as actions from './actions';
 import * as effects from './effects';
+
+// Modules
 import * as events from '../events';
 import * as tools from '../tools';
 import * as ui from '../ui';
+
 import { onInitialize } from './onInitialize';
 import { state } from './state';
 
@@ -30,21 +33,21 @@ export const config = merge(
   })
 );
 
-// declare module 'overmind' {
-//   // Due to circular typing we have to define an
-//   // explicit typing of state, actions and effects since
-//   // TS 3.9
-//   interface Config
-//     extends IConfig<{
-//       state: typeof config.state;
-//       actions: typeof config.actions;
-//       effects: typeof config.effects;
-//     }> {}
-// }
-
 declare module 'overmind' {
-  interface Config extends IConfig<typeof config> {}
+  // Due to circular typing we have to define an
+  // explicit typing of state, actions and effects since
+  // TS 3.9
+  interface Config
+    extends IConfig<{
+      state: typeof config.state;
+      actions: typeof config.actions;
+      effects: typeof config.effects;
+    }> {}
 }
+
+// declare module 'overmind' {
+//   type Config = IConfig<typeof config>;
+// }
 
 export const useApp = createHook<typeof config>();
 export const useState = createStateHook<typeof config>();
