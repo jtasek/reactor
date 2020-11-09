3348ab760deb6924b5a70ff99016758ab0898774
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-11-09 09:04:00 +0100

    Update state.ts

--- src/tools/state.ts
+++ src/tools/state.ts
@@ -2,6 +2,6 @@ import { activeTools } from './computed/activeTools';
 import { Tools } from './types';
 
 export const state: Tools = {
-  activeToolsIds: [],
-  activeTools
+  activeTools,
+  activeToolsIds: []
 };
