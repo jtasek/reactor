d1ecd29e2ff4f2125b790929c58214bd99621c8d
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-11-09 09:11:46 +0100

    Update types.ts

--- src/ui/types.ts
+++ src/ui/types.ts
@@ -7,103 +7,27 @@ export interface Control {
 }
 
 export interface UI {
-  commandLine: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  contextMenu: {
-    id: string;
-    name: string;
-    visible: boolean;
-    position: Position;
-  };
-  controlPanel: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  dataView: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  documentInfo: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  explorer: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  grid: {
-    id: string;
+  commandLine: Control;
+  contextMenu: Control & { position: Position };
+  controlPanel: Control;
+  dataView: Control;
+  documentInfo: Control;
+  explorer: Control;
+  grid: Control & {
     factor: number;
     height: number;
-    name: string;
-    visible: boolean;
     width: number;
   };
-  groupPanel: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  layerPanel: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  menuBar: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  miniMap: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  navBar: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  overlay: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  propertyPanel: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  searchBox: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  sideBar: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  statusBar: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  surface: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
-  toolBar: {
-    id: string;
-    name: string;
-    visible: boolean;
-  };
+  groupPanel: Control;
+  layerPanel: Control;
+  menuBar: Control;
+  miniMap: Control;
+  navBar: Control;
+  overlay: Control;
+  propertyPanel: Control;
+  searchBox: Control;
+  sideBar: Control;
+  statusBar: Control;
+  surface: Control;
+  toolBar: Control;
 }
