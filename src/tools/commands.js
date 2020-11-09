81f3d7bccf000843292759be69589653c522fa84
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-11-09 08:59:31 +0100

    Update commands.js

--- src/tools/commands.js
+++ src/tools/commands.js
@@ -1,62 +1,142 @@
-export default [
-  {
-    id: 'tools.clone',
-    command: 'clone(${id})',
-    description: 'Clones selected shape or group',
-    shortcut: 'ctrl+c'
+export const commands = {
+  clone: {
+    name: 'Clone',
+    description: 'Clones selection',
+    icon: {
+      group: 'content',
+      name: 'content_copy',
+      color: 'rgb(191, 34, 243)',
+      size: 24
+    },
+    factory: 'cloneShape',
+    regex: /(?<toolCode>clone)\('(?<shapeName>\w+)'\)/,
+    shortcut: 'ctrl+v',
+    type: 'clone'
   },
-  {
-    id: 'tools.circle',
-    command: 'circle(${cx, cy, r})',
+  circle: {
+    name: 'Circle',
     description: 'Draws circle',
-    shortcut: 'c'
+    icon: {
+      group: 'image',
+      name: 'panorama_fish_eye',
+      color: 'rgb(144, 254, 214)',
+      size: 24
+    },
+    factory: 'createCircle',
+    regex: /(?<toolCode>circle)\((?<cx>\d+),(?<cy>\d+),(?<radius>\d+)\)/,
+    shortcut: 'ctrl+c',
+    type: 'circle'
   },
-  {
-    id: 'tools.image',
-    command: 'image(${x, y, title, size, path})',
+  image: {
+    name: 'Image',
     description: 'Inserts image',
-    shortcut: 'i'
+    icon: {
+      group: 'image',
+      name: 'image',
+      color: 'rgb(234, 2, 130)',
+      size: 24
+    },
+    factory: 'insertImage',
+    regex: /(?<toolCode>image)\('(?<protocol>www|http|https):\/\/(?<url>[^\s]+[\w])'\)/,
+    shortcut: 'ctrl+i',
+    type: 'image'
   },
-  {
-    id: 'tools.line',
-    command: 'line(${cx, cy, c2x, c2y})',
+  line: {
+    name: 'Line',
     description: 'Draws a line',
-    shortcut: 'l'
+    icon: {
+      group: 'action',
+      name: 'timeline',
+      color: 'rgb(95, 216, 240)',
+      size: 24
+    },
+    factory: 'createLine',
+    regex: /(?<toolCode>line)\((?<x1>\d+),(?<y1>\d+),(?<x2>\d+),(?<y2>\d+)\)/,
+    shortcut: 'ctrl+l',
+    type: 'line'
   },
-  {
-    id: 'tools.move',
-    command: 'move(${id, x, y})',
-    description: 'Moves selected shape or group',
-    shortcut: 'm'
+  move: {
+    name: 'Move',
+    description: 'Moves selection',
+    icon: {
+      group: 'action',
+      name: 'visibility',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    factory: 'moveSelection',
+    regex: /(?<toolCode>move)\('(?<shapeName>\w+)',(?<x>\d+),(?<y>\d+)\)/,
+    shortcut: 'm',
+    type: 'move'
   },
-  {
-    id: 'tools.pen',
-    command: 'pen(${path})',
+  pen: {
+    name: 'Pen',
     description: 'Draws a curve line',
-    shortcut: 'p'
+    icon: {
+      group: 'content',
+      name: 'create',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    factory: 'pen',
+    regex: /(?<toolCode>pen)\((?<path>[\d, ]+)\)/,
+    shortcut: 'p',
+    type: 'pen'
   },
-  {
-    id: 'tools.rect',
-    command: 'rect(${cx, cy, c2x, c2y})',
+  rect: {
+    name: 'Rectangle',
     description: 'Draws a rectangle or square',
-    shortcut: 'r'
+    icon: {
+      group: 'image',
+      name: 'crop_square',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    factory: 'createRect',
+    regex: /(?<toolCode>rect)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
+    shortcut: 'r',
+    type: 'rect'
   },
-  {
-    id: 'tools.select',
-    command: 'select(${id})',
+  select: {
+    name: 'Select',
     description: 'Select a shape or group',
-    shortcut: 's'
+    icon: {
+      group: 'action',
+      name: 'pan_tool',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    factory: 'select',
+    regex: /(?<toolCode>select)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
+    shortcut: 's',
+    type: 'select'
   },
-  {
-    id: 'tools.text',
-    command: 'text(${x, y, text})',
+  text: {
+    name: 'Text',
     description: 'Types a text',
-    shortcut: 't'
+    icon: {
+      group: 'editor',
+      name: 'title',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    factory: 'typeText',
+    regex: /(?<toolCode>text)\((?<x>[\d]+),(?<y>[\d]+),'(?<text>[\w]+)'\)/,
+    shortcut: 't',
+    type: 'text'
   },
-  {
-    id: 'tools.zoom',
-    command: 'zoom(${factor})',
+  zoom: {
+    name: 'Zoom',
     description: 'Zooms in and out',
-    shortcut: 'z'
+    icon: {
+      group: 'action',
+      name: 'search',
+      color: 'rgba(255,255,255)',
+      size: 24
+    },
+    factory: 'zoom',
+    regex: /(?<toolCode>zoom)\((?<factor>[\d]+)\)/,
+    shortcut: 'z',
+    type: 'zoom'
   }
-];
+};
