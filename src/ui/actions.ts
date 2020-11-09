f8e749eae3dc80ce232f14429936b588446f5882
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-11-09 09:11:44 +0100

    Update actions.ts

--- src/ui/actions.ts
+++ src/ui/actions.ts
@@ -1,26 +1,35 @@
-import { Action } from 'overmind';
-import { UI } from './types';
+import { Action } from 'src/app/types';
 
-export const showControl: Action<string> = ({ controls }: UI, controlId: string) => {
-  controls[controlId].visible = true;
+import { Position } from '../app/types';
+
+export const displayContextMenu: Action<Position> = ({ state }, position) => {
+  state.ui.contextMenu.visible = true;
 };
 
-export const hideControl = ({ controls }: UI, controlId: string) => {
-  controls[controlId].visible = false;
+export const showControl: Action<string> = ({ state }, controlId: string) => {
+  state.ui[controlId].visible = true;
 };
 
-export const distructionFreeMode: Action<boolean> = ({ controls }: UI) => {
-  controls.commandLine.visible = false;
-  controls.contextMenu.visible = false;
-  controls.controlPanel.visible = false;
-  controls.groupPanel.visible = false;
-  controls.layerPanel.visible = false;
-  controls.menuBar.visible = false;
-  controls.minimap.visible = false;
-  controls.navBar.visible = false;
-  controls.propertyPanel.visible = false;
-  controls.sideBar.visible = false;
-  controls.statusBar.visible = false;
-  controls.surface.visible = false;
-  controls.toolBar.visible = false;
+export const hideControl: Action<string> = ({ state }, controlId: string) => {
+  state.ui[controlId].visible = false;
+};
+
+export const toggleControlVisibility: Action<string> = ({ state }, controlId: string) => {
+  state.ui[controlId].visible = !state.ui[controlId].visible;
+};
+
+export const distractionFreeMode: Action = ({ state }) => {
+  state.ui.commandLine.visible = false;
+  state.ui.contextMenu.visible = false;
+  state.ui.controlPanel.visible = false;
+  state.ui.groupPanel.visible = false;
+  state.ui.layerPanel.visible = false;
+  state.ui.menuBar.visible = false;
+  state.ui.miniMap.visible = false;
+  state.ui.navBar.visible = false;
+  state.ui.propertyPanel.visible = false;
+  state.ui.sideBar.visible = false;
+  state.ui.statusBar.visible = false;
+  state.ui.surface.visible = false;
+  state.ui.toolBar.visible = false;
 };
