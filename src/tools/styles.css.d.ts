fe8a9eed6bd6a6c4213a340105f250e53daeb759
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-11-09 09:04:22 +0100

    Create styles.css.d.ts

---
+++ src/tools/styles.css.d.ts
@@ -0,0 +1,13 @@
+declare namespace StylesCssNamespace {
+  export interface IStylesCss {
+    selected: string;
+    shape: string;
+  }
+}
+
+declare const StylesCssModule: StylesCssNamespace.IStylesCss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: StylesCssNamespace.IStylesCss;
+};
+
+export = StylesCssModule;
