c0e0367acf0819697d538b6e8cc7ac52edec8ef6
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2020-11-09 09:03:53 +0100

    Update index.ts

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,17 +1,20 @@
 import { state } from './state';
 import * as actions from './actions';
 
-import { Circle } from './components/Circle';
-import { Clone } from './components/Clone';
-import { Image } from './components/Image';
-import { Line } from './components/Line';
-import { Move } from './components/Move';
-import { Pen } from './components/Pen';
-import { Rectangle } from './components/Rectangle';
-import { Select } from './components/Select';
-import { Text } from './components/Text';
-import { Zoom } from './components/Zoom';
+import { Circle as circle } from './components/Circle';
+import { Image as image } from './components/Image';
+import { Line as line } from './components/Line';
+import { Pen as pen } from './components/Pen';
+import { Rect as rect } from './components/Rect';
+import { Text as text } from './components/Text';
 
-export { Circle, Clone, Image, Line, Move, Pen, Rectangle, Select, Text, Zoom };
+const components = {
+  circle,
+  image,
+  line,
+  pen,
+  rect,
+  text
+};
 
-export { state, actions };
+export { actions, state, components };
