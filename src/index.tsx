a16007e88cfaf2a2405ee942f15a3af13fb60af8
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2021-02-03 08:47:15 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from 'react-hot-loader';
 import { createOvermind } from 'overmind';
 import { Provider } from 'overmind-react';
 
 import { config } from './app';
-import { App } from './app/components/App';
 
 const overmind = createOvermind(config, {
   devtools: true
 });
 
-render(
-  <Provider value={overmind}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-);
+// render(
+//   <Provider value={overmind}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('app')
+// );
 
-// export const init = () => {
-//   const { Layout } = require('./modules/ui/components/Layout');
-//   render(
-//     <AppContainer>
-//       <Provider value={overmind}>
-//         <Layout />
-//         </Provider>
-//     </AppContainer>,
-//     document.getElementById('app');
-//   );
-// };
+export async function init(): Promise<void> {
+  const { App } = await import('./app/components/App');
+  render(
+    <AppContainer>
+      <Provider value={overmind}>
+        <App />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app')
+  );
+}
 
-// init();
+init();
 
-// if (module.hot) {
-//   module.hot.accept('./modules/ui/components/Layout', () =>
-//     requestAnimationFrame(() => init())
-//   );
-// }
+if (module.hot) {
+  module.hot.accept('./app/components/App', () => requestAnimationFrame(() => init()));
+}
