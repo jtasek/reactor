dd7c9abaa35d3fb0908b73e665641ef3877aebf4
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2021-10-10 01:02:47 +0200

    Update MiniMap.tsx

--- src/ui/components/MiniMap/MiniMap.tsx
+++ src/ui/components/MiniMap/MiniMap.tsx
@@ -1,6 +1,4 @@
 import React, { FC } from 'react';
-import { GridContainer } from '../Grid';
-
 import styles from './styles.css';
 
 import { Size } from 'src/app/types';
@@ -11,7 +9,6 @@ export interface Props {
 
 export const MiniMap: FC<Props> = ({ size, children }) => (
   <svg className={styles.minimap} width={size.width} height={size.height} viewBox="0 0 1000 1000">
-    <GridContainer key="map-grid" />
     {children}
   </svg>
 );
