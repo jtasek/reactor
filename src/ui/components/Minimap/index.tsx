11cc2cd4202f36ecdb8a0e03386c77735e28a22f
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2021-10-10 01:02:31 +0200

    Update index.tsx

--- src/ui/components/Minimap/index.tsx
+++ src/ui/components/Minimap/index.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
 import { useState } from 'src/app/hooks';
+import { Grid } from '../Grid/Grid';
 import { Rulers } from '../Surface/Rulers';
 import { Shapes } from '../Surface/Shapes';
 import { MiniMap } from './MiniMap';
+
+const STATIC_CAMERA = { position: { x: 0, y: 0 }, scale: 1 };
 
 export const MiniMapContainer: FC = () => {
   const { ui } = useState();
@@ -13,7 +16,8 @@ export const MiniMapContainer: FC = () => {
 
   return (
     <MiniMap size={{ width: 200, height: 200 }}>
-      <Rulers />
+      {ui.grid.visible && <Grid camera={STATIC_CAMERA} grid={ui.grid} />}
+      <Rulers key="minimap-rulers" camera={STATIC_CAMERA} />
       <Shapes />
     </MiniMap>
   );
