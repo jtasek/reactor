d7b282cbdc458ddaf46c132fb138a59f671712bf
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2021-10-15 09:28:58 +0200

    Add more hooks

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -13,3 +13,35 @@ export const useState = createStateHook<typeof config>();
 export const useActions = createActionsHook<typeof config>();
 export const useEffects = createEffectsHook<typeof config>();
 export const useReaction = createReactionHook<typeof config>();
+
+export const useKeyboardState = () => {
+  return useState().events.keyboard;
+};
+
+export const usePointerState = () => {
+  return useState().events.pointer;
+};
+
+export const useCurrentDocument = () => {
+  return useState().currentDocument;
+};
+
+export const useDocuments = () => {
+  return useState().documents ?? [];
+};
+
+export const useShapes = () => {
+  return useCurrentDocument()?.shapes ?? [];
+};
+
+export const useRulers = () => {
+  return useCurrentDocument()?.rulers ?? [];
+};
+
+export const useGroups = () => {
+  return useCurrentDocument()?.groups ?? [];
+};
+
+export const useLayers = () => {
+  return useCurrentDocument()?.layers ?? [];
+};
