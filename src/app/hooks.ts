ea36b0daddc39d801410f83a2d354de79003f600
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-02-03 00:43:37 +0100

    Add new hooks

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -12,6 +12,18 @@ export const useActions = createActionsHook<typeof config>();
 export const useEffects = createEffectsHook<typeof config>();
 export const useReaction = createReactionHook<typeof config>();
 
+export const useControls = () => {
+  return useState().ui;
+}
+
+export const useNavBar = () => {
+  return useControls().navBar;
+}
+
+export const useEvents = () => {
+  return useState().events;
+}
+
 export const useKeyboardState = () => {
   return useState().events.keyboard;
 };
@@ -24,20 +36,40 @@ export const useCurrentDocument = () => {
   return useState().currentDocument;
 };
 
+export const useDocument = (id: string) => {
+  return useState().documents[id];
+};
+
 export const useDocuments = () => {
   return useState().documents ?? [];
+};
+
+export const useShape = (id: string) => {
+  return useCurrentDocument()?.shapes[id];
 };
 
 export const useShapes = () => {
   return useCurrentDocument()?.shapes ?? [];
 };
 
+export const useRuler = (id: string) => {
+  return useCurrentDocument()?.rulers[id];
+};
+
 export const useRulers = () => {
   return useCurrentDocument()?.rulers ?? [];
 };
 
+export const useGroup = (id: string) => {
+  return useCurrentDocument()?.groups[id];
+};
+
 export const useGroups = () => {
   return useCurrentDocument()?.groups ?? [];
+};
+
+export const useLayer = (id: string) => {
+  return useCurrentDocument()?.layers[id];
 };
 
 export const useLayers = () => {
