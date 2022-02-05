cca58c238907e3ac05b535cee408e89ff1a2dfe3
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-02-05 11:17:26 +0100

    Update effects.ts

--- src/app/effects.ts
+++ src/app/effects.ts
@@ -1,23 +1,16 @@
 import page from 'page';
+
 export { v4 as newId } from 'uuid';
-
 export { execute } from './services/commandInterpreter';
-
 export { loadState, saveState } from './services/localStorage';
 
-// We allow void type which is used to define "no params"
-type IParams = {
-  [param: string]: string;
-} | void;
-
-export function registerRouters(routes: { [url: string]: (params: IParams) => void }): void {
+export const initializeRoutes = (routes) => {
   Object.keys(routes).forEach((url) => {
     page(url, ({ params }) => routes[url](params));
   });
-
   page.start();
-}
+};
 
-export function open(url: string): void {
+export const navigate = (url: string) => {
   page.show(url);
-}
+};
