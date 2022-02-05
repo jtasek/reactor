e7133a9ef88f9d59b8e61673cd181c8fa8e142fc
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-02-05 11:17:37 +0100

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,19 +3,15 @@ import { merge, namespaced } from 'overmind/config';
 import * as actions from './actions';
 import * as effects from './effects';
 
-// Modules
 import * as events from '../events';
 import * as tools from '../tools';
 import * as ui from '../ui';
-
-import { onInitialize } from './onInitialize';
 import { state } from './state';
 
 export const config = merge(
   {
     actions,
     effects,
-    onInitialize,
     state
   },
   namespaced({
