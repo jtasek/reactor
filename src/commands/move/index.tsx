0ec8810e1b658391e26bfb777f4a122e3db4247e
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-03-31 23:58:49 +0200

    Create move command

---
+++ src/commands/move/index.tsx
@@ -0,0 +1,21 @@
+import { Command } from 'src/app/types';
+import { Context } from 'src/app/hooks';
+
+export const moveSelection = (Context: Context, arg?: Record<string, unknown>) => {
+  console.log('TODO: Implement moveSelection');
+};
+
+export const MoveCommand: Command = {
+  id: 'move',
+  name: 'Move',
+  description: 'Move current selection',
+  icon: {
+    group: 'action',
+    name: 'open_with',
+    size: 24
+  },
+  regex: /(?<toolCode>move)\('(?<shapeName>\w+)',(?<x>\d+),(?<y>\d+)\)/,
+  shortcut: 'm',
+  canExecute: (context: Context) => true,
+  execute: moveSelection
+};
