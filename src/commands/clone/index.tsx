ccc9e38b6a3e7c93d86a3cb806ecdbe923bfb242
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-03-31 23:58:04 +0200

    Create clone command

---
+++ src/commands/clone/index.tsx
@@ -0,0 +1,37 @@
+import { Command } from 'src/app/types';
+import { Context } from 'src/app/hooks';
+import { createShape } from 'src/app/factories';
+
+const POSITION_SHIFT_STEP = 10;
+
+export const cloneSelection = ({ state }: Context) => {
+  state.currentDocument?.selectedShapes
+    .map((shape) =>
+      createShape({
+        ...shape,
+        id: undefined,
+        name: `Clone of ${shape.name}`,
+        position: {
+          x: shape.position.x + POSITION_SHIFT_STEP,
+          y: shape.position.y + POSITION_SHIFT_STEP
+        },
+        selected: false
+      })
+    )
+    .forEach((shape) => (state.currentDocument.shapes[shape.id] = shape));
+};
+
+export const CloneCommand: Command = {
+  id: 'clone',
+  name: 'Clone',
+  description: 'Clone current selection',
+  icon: {
+    group: 'content',
+    name: 'content_copy',
+    size: 24
+  },
+  regex: /(?<toolCode>clone)\('(?<shapeName>\w+)'\)/,
+  shortcut: 'ctrl+v',
+  canExecute: ({ state }: Context) => state.currentDocument?.selectedShapesIds.length > 0,
+  execute: cloneSelection
+};
