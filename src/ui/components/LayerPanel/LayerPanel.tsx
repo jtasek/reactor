44bb97ab352a03ebb24e379a85c72e07f4031e42
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-03-31 23:45:00 +0200

    Update layerpanel

--- src/ui/components/LayerPanel/LayerPanel.tsx
+++ src/ui/components/LayerPanel/LayerPanel.tsx
@@ -6,10 +6,13 @@ export interface Props {
   layersIds: string[];
 }
 
-export const LayerPanel: FC<Props> = ({ layersIds }) => (
-  <ul className={styles.layerPanel}>
-    {layersIds.map((layerId) => (
-      <LayerPanelItem key={layerId} layerId={layerId} />
-    ))}
-  </ul>
-);
+export const LayerPanel: FC<Props> = ({ layersIds }) => {
+  const showLayers = layersIds.length > 0;
+
+  return (
+    <ul className={styles.layerPanel}>
+      {!showLayers && <li>No layers available</li>}
+      {showLayers && layersIds.map((layerId) => <LayerPanelItem key={layerId} layerId={layerId} />)}
+    </ul>
+  );
+};
