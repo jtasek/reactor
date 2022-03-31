d44d302d650858ae12e881e46be5d14421a45434
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-03-31 23:57:30 +0200

    Update shapes.ts

--- src/app/computed/shapes.ts
+++ src/app/computed/shapes.ts
@@ -13,9 +13,22 @@ export const groupsIds = derived((currentDocument: Document) => {
   return Object.keys(currentDocument.groups);
 });
 
+export const selectedGroupsIds = derived((currentDocument: Document) => {
+  return Object.values(currentDocument.groups)
+    .filter((group) => group.selected)
+    .map((group) => group.id);
+});
+
 export const layersIds = derived((currentDocument: Document) => {
   return Object.keys(currentDocument.layers);
 });
+
+export const selectedLayersIds = derived((currentDocument: Document) => {
+  return Object.values(currentDocument.layers)
+    .filter((layer) => layer.selected)
+    .map((layer) => layer.id);
+});
+
 
 export const linksIds = derived((currentDocument: Document) => {
   return Object.keys(currentDocument.links);
