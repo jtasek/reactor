963fac383e7856bfeeb144888cdf4dc91f25a7b2
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-04-07 21:25:23 +0200

    Update shape.ts

--- src/app/actions/shape.ts
+++ src/app/actions/shape.ts
@@ -1,5 +1,6 @@
 import { Application, Action, Shape } from '../types';
 import { createShape } from '../factories';
+import { Context } from '../hooks';
 
 const getShape = ({ currentDocument }: Application, shapeId: string) => {
   const shape = currentDocument.shapes[shapeId];
@@ -20,59 +21,65 @@ const setShape = ({ currentDocument }: Application, shape: Shape) => {
 const deleteShape = ({ currentDocument }: Application, shapeId: string) =>
   delete currentDocument.shapes[shapeId];
 
-export const addShape: Action<Partial<Shape>> = ({ state }, options) => {
+export const addShape = ({ state }: Context, options: Partial<Shape>) => {
   const shape = createShape(options);
 
   setShape(state, shape);
 };
 
-export const cloneShape: Action<string> = ({ state, effects }, shapeId) => {
+export const cloneShape = ({ state, effects }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   setShape(state, { ...shape, id: effects.newId() });
 };
 
-export const removeShape: Action<string> = ({ state }, shapeId) => {
+export const removeShape = ({ state }: Context, shapeId: string) => {
   deleteShape(state, shapeId);
 };
 
-export const toggleShapeSelected: Action<string> = ({ state }, shapeId) => {
+export const toggleShapeSelected = ({ state }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   shape.selected = !shape.selected;
 };
 
-export const unselectShape: Action<string> = ({ state }, shapeId) => {
+export const selectShape = ({ state }: Context, shapeId: string) => {
+  const shape = getShape(state, shapeId);
+
+  shape.selected = true;
+};
+
+export const unselectShape = ({ state }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   shape.selected = false;
 };
 
-export const lockShape: Action<string> = ({ state }, shapeId) => {
+export const lockShape = ({ state }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   shape.locked = true;
 };
 
-export const unlockShape: Action<string> = ({ state }, shapeId) => {
+export const unlockShape = ({ state }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   shape.locked = false;
 };
 
-export const showShape: Action<string> = ({ state }, shapeId) => {
+export const showShape = ({ state }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   shape.visible = true;
 };
 
-export const hideShape: Action<string> = ({ state }, shapeId) => {
+export const hideShape = ({ state }: Context, shapeId: string) => {
   const shape = getShape(state, shapeId);
 
   shape.visible = false;
 };
 
-export const updateShape: Action<Partial<Shape> & { id: string }> = ({ state }, options) => {
+export const updateShape = ({ state }: Context, options: Partial<Shape> & { id: string }) => {
   const shape = getShape(state, options.id);
 
   setShape(state, { ...shape, ...options });
