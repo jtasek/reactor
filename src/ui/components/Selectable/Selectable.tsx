71bf5a63a9ed2dfb62a5776727681abd7986a9b6
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-04-11 10:40:26 +0200

    Update Selectable.tsx

--- src/ui/components/Selectable/Selectable.tsx
+++ src/ui/components/Selectable/Selectable.tsx
@@ -1,24 +1,34 @@
 import React, { FC } from 'react';
 import { useActions } from 'src/app/hooks';
-import { Shape } from 'src/app/types';
+import { Position, Shape, Size } from 'src/app/types';
 import { getBBox } from 'src/app/utils';
 import { Handle } from '../Handle';
 
 export const Resizable: FC<{ shape: Shape }> = ({ shape, children }) => {
   const bbox = getBBox(shape);
 
-  const topLeft = { x: bbox.point!.x, y: bbox.point!.y };
-  const topRight = { x: bbox.point!.x + bbox.size.width, y: bbox.point!.y };
-  const bottomLeft = { x: bbox.point!.x, y: bbox.point!.y + bbox.size.height };
-  const bottomRight = { x: bbox.point!.x + bbox.size.width, y: bbox.point!.y + bbox.size.height };
+  const {
+    topLeft,
+    topRight,
+    bottomLeft,
+    bottomRight,
+    middleLeft,
+    middleRight,
+    middleTop,
+    middleBottom
+  } = calcBoundingPoints(bbox);
 
   return (
     <>
       {children}
-      <Handle position={topLeft} />
-      <Handle position={topRight} />
-      <Handle position={bottomLeft} />
-      <Handle position={bottomRight} />
+      <Handle key="topLeft" position={topLeft} type="topLeft" shapeId={shape.id} />
+      <Handle key="topRight" position={topRight} type="topRight" shapeId={shape.id} />
+      <Handle key="bottomLeft" position={bottomLeft} type="bottomLeft" shapeId={shape.id} />
+      <Handle key="bottomRight" position={bottomRight} type="bottomRight" shapeId={shape.id} />
+      <Handle key="middleLeft" position={middleLeft} type="middleLeft" shapeId={shape.id} />
+      <Handle key="middleRight" position={middleRight} type="middleRight" shapeId={shape.id} />
+      <Handle key="middleTop" position={middleTop} type="middleTop" shapeId={shape.id} />
+      <Handle key="middleBottom" position={middleBottom} type="middleBottom" shapeId={shape.id} />
     </>
   );
 };
@@ -32,3 +42,31 @@ export const Selectable: FC<{ shape: Shape }> = ({ shape, children }) => {
     </Resizable>
   );
 };
+function calcBoundingPoints(bbox: { point: Position; size: Size }) {
+  const topLeft = { x: bbox.point!.x, y: bbox.point!.y };
+  const topRight = { x: bbox.point!.x + bbox.size.width, y: bbox.point!.y };
+  const bottomLeft = { x: bbox.point!.x, y: bbox.point!.y + bbox.size.height };
+  const bottomRight = { x: bbox.point!.x + bbox.size.width, y: bbox.point!.y + bbox.size.height };
+
+  const middleLeft = { x: bbox.point!.x, y: bbox.point!.y + bbox.size?.height / 2 };
+  const middleRight = {
+    x: bbox.point!.x + bbox.size.width,
+    y: bbox.point!.y + bbox.size?.height / 2
+  };
+  const middleTop = { x: bbox.point!.x + bbox.size?.width / 2, y: bbox.point!.y };
+  const middleBottom = {
+    x: bbox.point!.x + bbox.size?.width / 2,
+    y: bbox.point!.y + bbox.size.height
+  };
+
+  return {
+    topLeft,
+    topRight,
+    bottomLeft,
+    bottomRight,
+    middleLeft,
+    middleRight,
+    middleTop,
+    middleBottom
+  };
+}
