63b058e5119f396b1ebdcef93e7f805ff249c704
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-04-11 10:38:35 +0200

    Update handle

--- src/ui/components/Handle/index.tsx
+++ src/ui/components/Handle/index.tsx
@@ -1,12 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './styles.css';
 import type { Position } from '../../../app/types';
+import { useActions, useEvents } from 'src/app/hooks';
 
 export interface Props {
+  shapeId: string;
   position: Position;
   size?: number;
+  type:
+    | 'bottomLeft'
+    | 'bottomRight'
+    | 'middleBottom'
+    | 'middleLeft'
+    | 'middleRight'
+    | 'middleTop'
+    | 'topLeft'
+    | 'topRight';
 }
 
-export const Handle: FC<Props> = ({ position, size = 5 }) => (
-  <circle className={styles.handle} cx={position.x} cy={position.y} r={size} />
-);
+export const Handle: FC<Props> = ({ shapeId, position, type, size = 5 }) => {
+  const { resizeShape } = useActions();
+  const { pointer } = useEvents();
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    if (active) {
+      resizeShape({ shapeId, type, pointer });
+    }
+  }, [pointer.position]);
+
+  const handleMouseDown = (e) => {
+    setActive(true);
+  };
+
+  const handleMouseUp = (e) => {
+    setActive(false);
+  };
+
+  const classes = [styles.handle, styles[type]];
+
+  return (
+    <circle
+      className={classes.join(' ')}
+      cx={position.x}
+      cy={position.y}
+      r={size}
+      data-type={type}
+      fill={active ? 'red' : 'blue'}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    />
+  );
+};
