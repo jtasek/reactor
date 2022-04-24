54a9a4d59a3cf383092b362332ede704d9581e98
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-04-24 18:30:04 +0200

    Update Selectable.tsx

--- src/ui/components/Selectable/Selectable.tsx
+++ src/ui/components/Selectable/Selectable.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+
 import { useActions } from 'src/app/hooks';
 import { Position, Shape, Size } from 'src/app/types';
 import { getBBox } from 'src/app/utils';
@@ -6,6 +7,10 @@ import { Handle } from '../Handle';
 
 export const Resizable: FC<{ shape: Shape }> = ({ shape, children }) => {
   const bbox = getBBox(shape);
+
+  if (!bbox.point || !bbox.size) {
+    return children;
+  }
 
   const {
     topLeft,
@@ -36,12 +41,9 @@ export const Resizable: FC<{ shape: Shape }> = ({ shape, children }) => {
 export const Selectable: FC<{ shape: Shape }> = ({ shape, children }) => {
   const { toggleShapeSelected } = useActions();
 
-  return (
-    <Resizable shape={shape}>
-      <g onClick={() => toggleShapeSelected(shape.id)}>{children}</g>
-    </Resizable>
-  );
+  return <g onClick={() => toggleShapeSelected(shape.id)}>{children}</g>;
 };
+
 function calcBoundingPoints(bbox: { point: Position; size: Size }) {
   const topLeft = { x: bbox.point!.x, y: bbox.point!.y };
   const topRight = { x: bbox.point!.x + bbox.size.width, y: bbox.point!.y };
