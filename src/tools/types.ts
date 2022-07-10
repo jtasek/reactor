32ae14ec98d1214d6c03fb60e8ce6cb25352b2ec
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-07-10 10:49:07 +0200

    Update types.ts

--- src/tools/types.ts
+++ src/tools/types.ts
@@ -1,18 +1,13 @@
 import { FC } from 'react';
-import { Keyboard, Pointer } from 'src/events/types';
-import { Action, Icon } from '../app/types';
+
+import { Command } from '../app/types';
 
 export interface Tool {
   id: string;
   name: string;
   component?: FC<any>;
   description?: string;
-  factory?: (pointer: Pointer, keyboard: Keyboard) => any;
-  handler?: Action;
-  icon?: Icon;
-  regex: RegExp;
-  shortcut: string;
-  tool: FC<any>;
+  command: Command;
 }
 export interface Tools {
   activeToolsIds: string[];
