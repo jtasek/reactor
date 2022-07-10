4fa2e836153d62918b3612a17f823d7d6d5df3d4
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2022-07-10 17:44:30 +0200

    Update index.ts

--- src/tools/components/index.ts
+++ src/tools/components/index.ts
@@ -1,38 +1,36 @@
 import { FC } from 'react';
 import { Tool } from '../types';
 
-import { CircleCommand } from './Circle';
-import { EllipseCommand } from './Ellipse';
-import { ImageCommand } from './Image';
-import { LineCommand } from './Line';
-import { PenCommand } from './Pen';
-import { RectCommand } from './Rect';
-import { SelectCommand } from './Select';
-import { TextCommand } from './Text';
+import { CircleTool } from './Circle';
+import { EllipseTool } from './Ellipse';
+import { ImageTool } from './Image';
+import { LineTool } from './Line';
+import { PenTool } from './Pen';
+import { RectTool } from './Rect';
+import { SelectTool } from './Select';
+import { TextTool } from './Text';
 
-const library = [
-  CircleCommand,
-  EllipseCommand,
-  ImageCommand,
-  LineCommand,
-  PenCommand,
-  RectCommand,
-  SelectCommand,
-  TextCommand
+const tools = [
+  CircleTool,
+  EllipseTool,
+  ImageTool,
+  LineTool,
+  PenTool,
+  RectTool,
+  SelectTool,
+  TextTool
 ];
 
 export function getTool(toolId: string): Tool | undefined {
-  const result = library.find((item) => item.id === toolId);
-
-  return result;
+  return tools.find((item) => item.id === toolId);
 }
 
 export function getComponentByType(type: string): FC<any> | undefined {
-  const result = library.find((item) => item.id === type);
+  const result = tools.find((item) => item.id === type);
 
   return result?.component;
 }
 
 export function useRegisteredTools(): Tool[] {
-  return Object.values(library).map((item) => item);
+  return Object.values(tools).map((item) => item);
 }
