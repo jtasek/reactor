02b64f2978a7020cef60ca96c5a1c1c02369b8bd
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-01-23 23:20:12 +0100

    Update index.tsx

--- src/tools/components/Circle/index.tsx
+++ src/tools/components/Circle/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
-import { usePointer } from 'src/app/hooks';
-import type { Pointer } from 'src/events/types';
-import type { Tool } from 'src/tools/types';
+import React, { FC, useRef } from 'react';
+
 import styles from '../../styles.css';
+import type { Tool } from 'src/tools/types';
+import { Command } from 'src/app/types';
+import { Context } from 'src/app/hooks';
 
 /**
  * Draws a circle based on input centre point and radius
@@ -24,12 +25,14 @@ interface Props {
   type: string;
 }
 
-export const createCircle = ({ centre, radius }: Pointer): Props => {
+export const createCircleProps = ({ state }: Context, designMode = false) => {
+  const { centre, scaledCentre, radius, scaledRadius } = state.events.pointer;
+
   return {
-    code: 'circle',
-    cx: centre.x,
-    cy: centre.y,
-    r: radius,
+    code: 'circle-x',
+    cx: designMode ? scaledCentre.x : centre.x,
+    cy: designMode ? scaledCentre.y : centre.y,
+    r: designMode ? scaledRadius : radius,
     name: 'Circle x',
     selected: true,
     type: 'circle'
@@ -37,17 +40,23 @@ export const createCircle = ({ centre, radius }: Pointer): Props => {
 };
 
 export const Circle: FC<Props> = ({ code, cx, cy, r, selected }) => {
+  // const shape = useRef();
   const className = selected ? `${styles.shape} ${styles.selected}` : styles.shape;
 
   return (
     <circle
+      //   ref={shape}
       key="circle"
       fill="none"
       stroke="grey"
       strokeWidth={2}
       data-cy={code}
+      data-name={name}
       className={className}
-      onClick={() => alert('CIRCLE')}
+      onClick={() => {
+        // console.log('circle bbox', shape.current.getBBox());
+        //  console.log(`x: ${shape.current?.x}, y: ${shape.current?.y}`);
+      }}
       cx={cx}
       cy={cy}
       r={r}
@@ -55,25 +64,29 @@ export const Circle: FC<Props> = ({ code, cx, cy, r, selected }) => {
   );
 };
 
-export const CircleTool: FC = () => {
-  const pointer = usePointer();
-
-  return <Circle key="circle" {...createCircle(pointer)} />;
+export const CircleCommand: Command = {
+  id: 'circle',
+  name: 'Circle',
+  category: 'shapes',
+  regex: /(?<toolCode>circle)\((?<cx>\d+),(?<cy>\d+),(?<radius>\d+)\)/,
+  shortcut: 'ctrl+c',
+  canExecute: (context, args?) => true,
+  execute: (context: Context, args?) =>
+    React.createElement(Circle, createCircleProps(context), null),
+  factory: (context: Context) => createCircleProps(context, true)
 };
 
-export const CircleCommand: Tool = {
+// Tool is visual command that is used to draw a shape
+export const CircleTool: Tool = {
   id: 'circle',
   name: 'Circle',
   description: 'Draw a circle',
-  factory: createCircle,
-  tool: CircleTool,
+  command: CircleCommand,
   component: Circle,
   icon: {
     group: 'image',
     name: 'panorama_fish_eye',
-    // color: 'rgb(144, 254, 214)',
+    color: 'rgb(144, 254, 214)',
     size: 24
-  },
-  regex: /(?<toolCode>circle)\((?<cx>\d+),(?<cy>\d+),(?<radius>\d+)\)/,
-  shortcut: 'ctrl+c'
+  }
 };
