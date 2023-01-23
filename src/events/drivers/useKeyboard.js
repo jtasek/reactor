f501d91b83504f1b1ab61d82abc8c51889e67526
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-01-23 23:19:43 +0100

    Create useKeyboard.js

---
+++ src/events/drivers/useKeyboard.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { Shell } from 'src/app/components/Shell';
+import { useActions } from 'src/app/hooks';
+
+export const useKeyboard = () => {
+  const actions = useActions();
+  useEffect(() => {
+    const keyDownListener = window.addEventListener('keydown', actions.events.keyDown);
+    const keyupListener = window.addEventListener('keyup', actions.events.keyUp);
+
+    return () => {
+      window.removeEventListener(keyDownListener);
+      window.removeEventListener(keyupListener);
+    };
+  }, []);
+};
