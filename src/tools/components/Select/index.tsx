90759544ee442cd66cc2e835595810e408d9a337
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-01-23 23:20:43 +0100

    Update index.tsx

--- src/tools/components/Select/index.tsx
+++ src/tools/components/Select/index.tsx
@@ -1,51 +1,41 @@
 import React, { FC } from 'react';
 import inlineStyles from './inlineStyles';
-import type { Command, Point, Size } from 'src/app/types';
+import type { Point, Size } from 'src/app/types';
 import type { Tool } from 'src/tools/types';
 import { usePointer } from 'src/app/hooks';
+import { SelectCommand } from 'src/commands/select';
 
 /**
  * Selects highlighted shapes
  **/
 
 interface Props {
-  topLeftPosition: Point;
+  position: Point;
   size: Size;
 }
 
-export const Select: FC<Props> = ({ topLeftPosition, size }) => (
+export const Select: FC<Props> = ({ position, size }) => (
   <rect
     key="selection"
     style={inlineStyles}
-    x={topLeftPosition.x}
-    y={topLeftPosition.y}
+    x={position.x}
+    y={position.y}
     width={size.width}
     height={size.height}
     strokeDasharray="5, 5"
   />
 );
 
-export const SelectTool: FC = () => {
-  const pointer = usePointer();
-
-  return <Select {...pointer} />;
-};
-
-export const SelectCommand: Tool = {
+export const SelectTool: Tool = {
   id: 'select',
   name: 'Select',
   description: 'Select a shape or group',
-  factory: (): Shape => {
-    throw new Error('Factory not implemented');
-  },
-  tool: SelectTool,
+  command: SelectCommand,
   component: Select,
   icon: {
     group: 'action',
     name: 'pan_tool',
-    // color: 'rgba(255,255,255)',
+    color: 'rgba(255,255,255)',
     size: 24
-  },
-  regex: /(?<toolCode>select)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
-  shortcut: 's'
+  }
 };
