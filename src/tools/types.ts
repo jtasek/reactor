0b0057f5668ad517d7cbef0d0bc74445445fd3ec
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-01-23 23:21:10 +0100

    Update types.ts

--- src/tools/types.ts
+++ src/tools/types.ts
@@ -3,11 +3,11 @@ import { FC } from 'react';
 import { Command } from '../app/types';
 
 export interface Tool {
+  command: Command;
+  component: FC<any>;
+  description?: string;
   id: string;
   name: string;
-  component?: FC<any>;
-  description?: string;
-  command: Command;
 }
 export interface Tools {
   activeToolsIds: string[];
