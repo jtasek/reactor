9418bb316355ce12fbbb1c781a6d05e75d17c5ec
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-01-23 23:18:46 +0100

    Update index.tsx

--- src/commands/select/index.tsx
+++ src/commands/select/index.tsx
@@ -21,6 +21,7 @@ export const SelectCommand: Command = {
   },
   regex: /(?<toolCode>select)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
   shortcut: 's',
-  canExecute: (context: Context) => true,
-  execute: selectShape
+  canExecute: (context: Context, args) => true,
+  execute: selectShape,
+  factory: (context: Context) => createSelectProps(context, true)
 };
