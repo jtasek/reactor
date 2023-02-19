e2d03818d86715d57818aeb3f8b5065c0d4d887e
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-02-19 21:39:34 +0100

    Update utils.test.ts

--- src/app/__tests__/utils.test.ts
+++ src/app/__tests__/utils.test.ts
@@ -1,5 +1,13 @@
-import { Box, Point, Rectangle, Shape } from '../types';
-import { getDistance, overlaps, pointInRectangle } from '../utils';
+import { Box, Point, Shape } from '../types';
+import {
+  Ellipse,
+  getDistance,
+  isCircleInBox,
+  isEllipseInBox,
+  isRectangleInBox,
+  overlaps,
+  isPointInBox
+} from '../utils';
 
 describe('utils', () => {
   describe('getDistance()', () => {
@@ -12,20 +20,122 @@ describe('utils', () => {
     });
   });
 
-  describe('pointInRectangle()', () => {
+  describe('isCircleInBox', () => {
+    const center: Point = { x: 5, y: 5 };
+    const r = 2;
+
+    it('should return true if circle is contained in box', () => {
+      const box: Box = {
+        topLeft: { x: 0, y: 0 },
+        bottomRight: { x: 10, y: 10 }
+      };
+      expect(isCircleInBox(center, r, box)).toBe(true);
+    });
+
+    it('should return false if circle is not contained in box', () => {
+      const box: Box = {
+        topLeft: { x: 0, y: 0 },
+        bottomRight: { x: 4, y: 4 }
+      };
+      expect(isCircleInBox(center, r, box)).toBe(false);
+    });
+
+    it('should return false if circle center is outside box', () => {
+      const box: Box = {
+        topLeft: { x: 6, y: 6 },
+        bottomRight: { x: 10, y: 10 }
+      };
+      expect(isCircleInBox(center, r, box)).toBe(false);
+    });
+
+    it('should return false if circle intersects box', () => {
+      const box: Box = {
+        topLeft: { x: 3, y: 3 },
+        bottomRight: { x: 10, y: 10 }
+      };
+      expect(isCircleInBox(center, r, box)).toBe(false);
+    });
+  });
+
+  describe('isRectangleInBox', () => {
+    const box1: Box = {
+      topLeft: { x: 2, y: 2 },
+      bottomRight: { x: 6, y: 6 }
+    };
+    const box2: Box = {
+      topLeft: { x: 3, y: 3 },
+      bottomRight: { x: 5, y: 5 }
+    };
+    const box3: Box = {
+      topLeft: { x: 4, y: 4 },
+      bottomRight: { x: 8, y: 8 }
+    };
+
+    test('Box inside box should return true', () => {
+      expect(isRectangleInBox(box2, box1)).toBe(true);
+    });
+
+    test('Box outside box should return false', () => {
+      expect(isRectangleInBox(box3, box1)).toBe(false);
+    });
+
+    test('Box partially inside box should return false', () => {
+      const box4: Box = {
+        topLeft: { x: 1, y: 1 },
+        bottomRight: { x: 4, y: 4 }
+      };
+      expect(isRectangleInBox(box4, box1)).toBe(false);
+    });
+  });
+
+  describe('isEllipseInBox', () => {
+    const ellipse: Ellipse = {
+      center: { x: 5, y: 5 },
+      radiusX: 2,
+      radiusY: 3
+    };
+    const box: Box = {
+      topLeft: { x: 2, y: 2 },
+      bottomRight: { x: 8, y: 8 }
+    };
+
+    test('Ellipse inside box should return true', () => {
+      expect(isEllipseInBox(ellipse, box)).toBe(true);
+    });
+
+    test('Ellipse outside box should return false', () => {
+      const ellipse2: Ellipse = {
+        center: { x: 10, y: 10 },
+        radiusX: 2,
+        radiusY: 3
+      };
+      expect(isEllipseInBox(ellipse2, box)).toBe(false);
+    });
+
+    test('Ellipse partially outside box should return false', () => {
+      const ellipse3: Ellipse = {
+        center: { x: 6, y: 6 },
+        radiusX: 3,
+        radiusY: 4
+      };
+      expect(isEllipseInBox(ellipse3, box)).toBe(false);
+    });
+  });
+
+  describe('isPointInBox()', () => {
     it('returns true when point is inside of rectangle', () => {
-      const actual = pointInRectangle(
+      const actual = isPointInBox(
         { x: 50, y: 50 },
-        { p: { x: 1, y: 1 }, size: { width: 100, height: 100 } }
+        { position: { x: 1, y: 1 }, size: { width: 100, height: 100 } }
       );
 
       expect(actual).toBe(true);
     });
 
     it('returns false when point is outside of rectangle', () => {
-      const actual = pointInRectangle(
+      const actual = isPointInBox(
         { x: 1, y: 1 },
-        { p: { x: 50, y: 50 }, size: { width: 100, height: 100 } }
+        { position: { x: 50, y: 50 }, size: { width: 100, height: 100 } }
       );
 
       expect(actual).toBe(false);
@@ -43,7 +153,7 @@ describe('utils', () => {
     });
   });
 
-  describe('pointInRect()', () => {
+  describe('isPointInBox()', () => {
     it('returns true when point is withing the box boundaries', () => {
       const point: Point = { x: 100, y: 100 };
 
@@ -52,7 +162,7 @@ describe('utils', () => {
         size: { height: 100, width: 100 }
       };
 
-      const actual = pointInRectangle(point, shape);
+      const actual = isPointInBox(point, shape);
 
       expect(actual).toBe(false);
     });
