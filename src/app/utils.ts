9384ddc8feb588fd1d18ad294c1c71d0d2e5a97c
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-02-19 21:38:45 +0100

    Update utils.ts

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,4 +1,4 @@
-import { Box, Point, Shape, Vector } from './types';
+import { Box, Circle, Line, Point, Shape, Vector } from './types';
 
 export function stringifyPath(path: Point[]): string {
   return path.map((point: Point) => `${point.x}, ${point.y}`).join(' ');
@@ -16,37 +16,52 @@ export function getDistance(p1: Point, p2: Point): number {
   return Math.hypot(Math.abs(p2.x - p1.x), Math.abs(p2.y - p1.y));
 }
 
-export function getBBox(shape: Partial<Shape>): Box | undefined {
+function getBoundingBoxForRectangle(rectangle: Box): Box {
+  return rectangle;
+}
+
+function getBoundingBoxForCircle(circle: Circle): Box {
+  debugger;
+  return {
+    topLeft: { x: circle.center.x - circle.radius, y: circle.center.y - circle.radius },
+    bottomRight: { x: circle.center.x + circle.radius, y: circle.center.y + circle.radius }
+  };
+}
+
+function getBoundingBoxForEllipse(ellipse: Ellipse): Box {
+  return {
+    topLeft: { x: ellipse.center.x - ellipse.radiusX, y: ellipse.center.y - ellipse.radiusY },
+    bottomRight: { x: ellipse.center.x + ellipse.radiusX, y: ellipse.center.y + ellipse.radiusY }
+  };
+}
+
+function getBoundingBoxForLine(line: Line): Box {
+  return { topLeft: line.start, bottomRight: line.end };
+}
+
+export function getBoundingBox(shape: Partial<Shape>): Box | undefined {
   if (!shape) {
     return undefined;
   }
 
   if (shape.type === 'line') {
-    //@ts-ignore
-    return { topLeft: { x: shape.x1, y: shape.y1 }, bottomRight: { x: shape.x2, y: shape.y2 } };
+    return getBoundingBoxForLine(shape as Line);
   }
 
   if (shape.type === 'circle') {
-    return {
-      topLeft: { x: shape.cx! - shape.r!, y: shape.cy! - shape.r! },
-      bottomRight: {
-        x: shape.cx + shape.r!,
-        y: shape.cy + shape.r!
-      }
-    };
+    return getBoundingBoxForCircle(shape as Circle);
   }
 
-  return {
-    topLeft: shape.position as Point,
-    bottomRight: {
-      x: (shape.position as Point).x + (shape as Point).size!.width,
-      y: (shape.position as Point).y + (shape as Point).size!.height
-    }
-  };
+  if (shape.type === 'ellipse') {
+    return getBoundingBoxForEllipse(shape as Ellipse);
+  }
+
+  // image, rect, text
+  return getBoundingBoxForRectangle(shape as Box);
 }
 
-export function pointInRectangle(p: Point, shape: Partial<Shape>): boolean {
-  const box = getBBox(shape);
+export function isPointInBox(p: Point, shape: Partial<Shape>): boolean {
+  const box = getBoundingBox(shape);
 
   if (!box) {
     return false;
@@ -56,6 +71,80 @@ export function pointInRectangle(p: Point, shape: Partial<Shape>): boolean {
   const verticalFit = box.topLeft!.y <= p.y && p.y <= box.bottomRight.y;
 
   return horizontalFit && verticalFit;
+}
+
+export function isCircleInBox(center: Point, r: number, box: Box): boolean {
+  // Step 1: Check if the center of the circle is inside the rectangle
+  if (
+    center.x < box.topLeft.x ||
+    center.x > box.bottomRight.x ||
+    center.y < box.topLeft.y ||
+    center.y > box.bottomRight.y
+  ) {
+    return false;
+  }
+
+  // Step 2: Calculate the four points on the perimeter of the circle
+  const top_point = { x: center.x, y: center.y - r };
+  const bottom_point = { x: center.x, y: center.y + r };
+  const left_point = { x: center.x - r, y: center.y };
+  const right_point = { x: center.x + r, y: center.y };
+
+  // Step 3: Check if all the points are inside the rectangle
+  return (
+    top_point.x >= box.topLeft.x &&
+    top_point.x <= box.bottomRight.x &&
+    top_point.y >= box.topLeft.y &&
+    top_point.y <= box.bottomRight.y &&
+    bottom_point.x >= box.topLeft.x &&
+    bottom_point.x <= box.bottomRight.x &&
+    bottom_point.y >= box.topLeft.y &&
+    bottom_point.y <= box.bottomRight.y &&
+    left_point.x >= box.topLeft.x &&
+    left_point.y >= box.topLeft.y &&
+    left_point.y <= box.bottomRight.y &&
+    right_point.x <= box.bottomRight.x &&
+    right_point.y >= box.topLeft.y &&
+    right_point.y <= box.bottomRight.y
+  );
+}
+
+export function isRectangleInBox(box1: Box, box2: Box): boolean {
+  // Check if box1 is contained within box2
+  return (
+    box1.topLeft.x >= box2.topLeft.x &&
+    box1.bottomRight.x <= box2.bottomRight.x &&
+    box1.topLeft.y >= box2.topLeft.y &&
+    box1.bottomRight.y <= box2.bottomRight.y
+  );
+}
+
+export interface Ellipse {
+  center: Point;
+  radiusX: number;
+  radiusY: number;
+}
+
+export function isEllipseInBox(ellipse: Ellipse, box: Box): boolean {
+  // Calculate the left, right, top, and bottom edges of the rectangle
+  const leftEdge = box.topLeft.x;
+  const rightEdge = box.bottomRight.x;
+  const topEdge = box.topLeft.y;
+  const bottomEdge = box.bottomRight.y;
+
+  // Calculate the boundaries of the ellipse
+  const leftEllipse = ellipse.center.x - ellipse.radiusX;
+  const rightEllipse = ellipse.center.x + ellipse.radiusX;
+  const topEllipse = ellipse.center.y - ellipse.radiusY;
+  const bottomEllipse = ellipse.center.y + ellipse.radiusY;
+
+  // Check if the boundaries of the ellipse are inside the rectangle
+  return (
+    leftEllipse >= leftEdge &&
+    rightEllipse <= rightEdge &&
+    topEllipse >= topEdge &&
+    bottomEllipse <= bottomEdge
+  );
 }
 
 export function overlaps(source?: Box, target?: Box) {
