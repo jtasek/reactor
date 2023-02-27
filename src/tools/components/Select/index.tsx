c19ac90d4d826578838cf99f483f4fda0daed74c
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-02-27 21:52:35 +0100

    Update index.tsx

--- src/tools/components/Select/index.tsx
+++ src/tools/components/Select/index.tsx
@@ -2,8 +2,10 @@ import React, { FC } from 'react';
 import inlineStyles from './inlineStyles';
 import type { Point, Size } from 'src/app/types';
 import type { Tool } from 'src/tools/types';
-import { usePointer } from 'src/app/hooks';
-import { SelectCommand } from 'src/commands/select';
+import { Command } from 'src/app/types';
+import { Context } from 'src/app/hooks';
+import { isPointInBox } from 'src/app/utils';
+import { log } from 'console';
 
 /**
  * Selects highlighted shapes
@@ -25,6 +27,46 @@ export const Select: FC<Props> = ({ position, size }) => (
     strokeDasharray="5, 5"
   />
 );
+
+export function createSelectProps({ state }: Context, designMode = false) {
+  const { topLeftPosition, scaledTopLeftPosition, size, scaledSize } = state.events.pointer;
+
+  return {
+    key: 'selection',
+    name: 'selection',
+    position: designMode ? scaledTopLeftPosition : topLeftPosition,
+    size: designMode ? scaledSize : size,
+    type: 'select'
+  };
+}
+
+export const selectShape = ({ state }: Context) => {
+  const { events, currentDocument } = state;
+  Object.values(currentDocument.shapes).forEach((shape) =>
+    isPointInBox(events.pointer.position, shape)
+  );
+};
+
+export const SelectCommand: Command = {
+  id: 'select',
+  name: 'Select',
+  category: 'shapes',
+  description: 'Selects shapes and groups ',
+  icon: {
+    group: 'action',
+    name: 'pan_tool',
+    color: 'rgba(255,255,255)',
+    size: 24
+  },
+  regex: /(?<toolCode>select)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
+  shortcut: 's',
+  canExecute: (context: Context, args) => true,
+  execute: (context, args) => {
+    console.log('select executer');
+    React.createElement(Select, createSelectProps(context) as Props, null);
+  },
+  factory: (context: Context) => createSelectProps(context, true)
+};
 
 export const SelectTool: Tool = {
   id: 'select',
