deb7d2f380c4a2f9c9ab6bdc6f73f3a7d51dec46
Jakub Tašek <jakub.tasek@external.erstegroup.com>
2023-06-14 22:40:01 +0200

    Update index.tsx

--- src/ui/components/Surface/index.tsx
+++ src/ui/components/Surface/index.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
-import { Stack } from 'src/tools/components/Stack';
-import { GridContainer as Grid } from '../Grid';
-import { ConnectedOverlay as Overlay } from '../Overlay';
 import { Camera } from './Camera';
+import { ConnectedOverlay as Overlay } from '../Overlay';
+import { ErrorBoundary } from '../ErrorBoundary';
+import { GridContainer as Grid } from '../Grid';
 import { Rulers } from './Rulers';
 import { Shapes } from './Shapes';
+import { Stack } from 'src/tools/components/Stack';
 import { Surface } from './Surface';
 
 export const SurfaceContainer: FC = () => (
@@ -14,7 +15,9 @@ export const SurfaceContainer: FC = () => (
     <Camera>
       <Shapes />
     </Camera>
-    <Stack />
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <Stack />
+    </ErrorBoundary>
     <Overlay />
   </Surface>
 );
