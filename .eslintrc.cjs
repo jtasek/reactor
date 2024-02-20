58505dbcf77465539e8dc4a753d0365fc2d3d940
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-21 00:28:18 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,8 +11,8 @@ module.exports =  {
     'plugin:jest/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
