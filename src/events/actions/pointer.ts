1556d0f794044298edb239f91bd90423930746eb
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-21 00:17:42 +0100

    Update pointer.ts

--- src/events/actions/pointer.ts
+++ src/events/actions/pointer.ts
@@ -1,36 +1,36 @@
 import { Action, Position } from '../../app/types';
 
-export const setStartPosition: Action<Position> = ({ state }, coords) => {
-  state.events.pointer.startPosition = coords;
+export const setStartPosition: Action<Position> =  ({ state }, position) => {
+  state.events.pointer.startPosition = position;
 };
 
-export const setCurrentPosition: Action<Position> = ({ state }, coords) => {
-  state.events.pointer.position = coords;
+export const setCurrentPosition: Action<Position> = ({ state }, position) => {
+  state.events.pointer.position = position;
 };
 
-export const updatePath: Action<Position> = ({ state }, coords) => {
-  state.events.pointer.path.push(coords);
+export const updatePath: Action<Position> = ({ state }, position) => {
+  state.events.pointer.path.push(position);
 };
 
-export const startDragging: Action<Position> = ({ state }, coords) => {
+export const startDragging: Action<Position> = ({ state }, position) => {
   const pointer = state.events.pointer;
   pointer.dragging = true;
-  pointer.startPosition = coords;
-  pointer.position = coords;
-  pointer.path = [coords];
+  pointer.startPosition = position;
+  pointer.position = position;
+  pointer.path = [position];
 };
 
-export const dragging: Action<Position> = ({ state }, coords) => {
+export const dragging: Action<Position>  = ({ state }, position) => {
   const pointer = state.events.pointer;
-  pointer.position = coords;
-  pointer.path.push(coords);
+  pointer.position = position;
+  pointer.path.push(position);
 };
 
-export const endDragging: Action = ({ state }) => {
+export const endDragging: Action<Position>  = ({ state }) => {
   state.events.pointer.dragging = false;
 };
 
-export const resetDragging: Action = ({ state }) => {
+export const resetDragging: Action<Position> = ({ state }) => {
   const pointer = state.events.pointer;
   pointer.dragging = false;
   pointer.startPosition = { x: 0, y: 0 };
