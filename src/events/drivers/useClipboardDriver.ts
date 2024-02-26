efa5555516d2630ad1a3ba101ae979b1cf81d002
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-27 00:08:52 +0100

    Update useClipboardDriver.ts

--- src/events/drivers/useClipboardDriver.ts
+++ src/events/drivers/useClipboardDriver.ts
@@ -3,11 +3,14 @@ import { useActions } from 'src/app/hooks';
 export const useKeyboardDriver = () => {
   const actions = useActions();
 
-  const handleCopy = (event) => {};
-  const handleCut = (event) => {};
-  const handlePaste = (event) => {};
+  const handleCopy = (event) => {
+  };
+  const handleCut = (event) => {
+  };
+  const handlePaste = (event) => {
+  };
 
-  return () => {
-    handleCopy, handleCut, handlePaste;
+  return  {
+    handleCopy, handleCut, handlePaste
   };
 };
