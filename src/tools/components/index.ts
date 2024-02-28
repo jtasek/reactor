4f1e41eae14c8f86c3097f9de444e5ad11df9d64
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-28 22:57:05 +0100

    Update index.ts

--- src/tools/components/index.ts
+++ src/tools/components/index.ts
@@ -11,30 +11,30 @@ import { SelectTool } from './Select';
 import { TextTool } from './Text';
 
 const tools = [
-  CircleTool,
-  EllipseTool,
-  ImageTool,
-  LineTool,
-  PenTool,
-  RectTool,
-  SelectTool,
-  TextTool
+    CircleTool,
+    EllipseTool,
+    ImageTool,
+    LineTool,
+    PenTool,
+    RectTool,
+    SelectTool,
+    TextTool
 ];
 
 export function getToolById(toolId: string): Tool | undefined {
-  return tools.find((item) => item.id === toolId);
+    return tools.find((item) => item.id === toolId);
 }
 
-export function getComponentByType(type: string): FC<any> | undefined {
-  const result = tools.find((item) => item.id === type);
+export function getComponentByType(type: string) {
+    const result = getToolById(type);
 
-  if (!result?.component) {
-    throw new Error(`${type} type doesn't have component defined`);
-  }
+    if (!result?.component) {
+        throw new Error(`${type} type doesn't have component defined`);
+    }
 
-  return result.component;
+    return result.component;
 }
 
 export function useRegisteredTools(): Tool[] {
-  return Object.values(tools).map((item) => item);
+    return Object.values(tools).map((item) => item);
 }
