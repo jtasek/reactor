f0f6b6f32c1e9814750303f222c684c576e65c90
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-28 22:59:34 +0100

    Update Shape.tsx

--- src/ui/components/Shape/Shape.tsx
+++ src/ui/components/Shape/Shape.tsx
@@ -1,38 +1,38 @@
-import React, { FC } from 'react';
+import { createElement, FC, ReactNode } from 'react';
 import { useShape } from 'src/app/hooks';
-import { getComponentByType } from 'src/tools/components';
+import { getComponentByType, getToolById } from 'src/tools/components';
 import { Resizable, Selectable } from '../Selectable/Selectable';
 
 interface Props {
-  shapeId: string;
+    shapeId: string;
 }
 
 export const Shape: FC<Props> = ({ shapeId }) => {
-  const shape = useShape(shapeId);
-  const component = getComponentByType(shape.type);
+    const shape = useShape(shapeId);
+    const component = getComponentByType(shape.type);
 
-  if (!component) {
-    console.error(`Component ${shape.type} not found`);
-    return null;
-  }
+    if (!component) {
+        console.error(`Component ${shape.type} not found`);
+        return null;
+    }
 
-  if (shape.selected) {
-    return React.createElement(
-      Resizable,
-      {
-        shape: shape,
-        key: `${shape.type}-${shape.id}`
-      },
-      React.createElement(component, shape)
+    if (shape.selected) {
+        return createElement(
+            Resizable,
+            {
+                shape: shape,
+                key: `${shape.type}-${shape.id}`
+            },
+            createElement(component, shape)
+        );
+    }
+
+    return createElement(
+        Selectable,
+        {
+            shape: shape,
+            key: `${shape.type}-${shape.id}`
+        },
+        createElement(component, shape)
     );
-  }
-
-  return React.createElement(
-    Selectable,
-    {
-      shape: shape,
-      key: `${shape.type}-${shape.id}`
-    },
-    React.createElement(component, shape)
-  );
 };
