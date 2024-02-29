922b1d6d70941318557444bfd88bc64e8400500c
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-29 01:54:42 +0100

    Update index.tsx

--- src/tools/components/Select/index.tsx
+++ src/tools/components/Select/index.tsx
@@ -1,76 +1,93 @@
 import React, { FC } from 'react';
+
 import inlineStyles from './inlineStyles';
-import type { Point, Size } from 'src/app/types';
+import type { Position, Size } from 'src/app/types';
 import type { Tool } from 'src/tools/types';
 import { Command } from 'src/app/types';
+import { Pointer } from '../../../events/types';
 import { isPointInBox } from 'src/app/utils';
-import { Context } from '../../../app';
+import { usePointer } from '../../../app/hooks';
 
 /**
  * Selects highlighted shapes
  **/
 
 interface Props {
-  position: Point;
-  size: Size;
+    key: string;
+    name: string;
+    position: Position;
+    size: Size;
+    type: string;
 }
 
-export const Select: FC<Props> = ({ position, size }) => (
-  <rect
-    key="selection"
-    style={inlineStyles}
-    x={position.x}
-    y={position.y}
-    width={size.width}
-    height={size.height}
-    strokeDasharray="5, 5"
-  />
-);
-
-export function createSelectProps({ state }: Context, designMode = false) {
-  const { topLeftPosition, scaledTopLeftPosition, size, scaledSize } = state.events.pointer;
-
-  return {
-    key: 'selection',
-    name: 'selection',
-    position: designMode ? scaledTopLeftPosition : topLeftPosition,
-    size: designMode ? scaledSize : size,
-    type: 'select'
-  };
+export function createSelectProps(
+    { topLeftPosition, scaledTopLeftPosition, size, scaledSize }: Pointer,
+    designMode = false
+): Props {
+    return {
+        key: 'selection',
+        name: 'selection',
+        position: designMode ? scaledTopLeftPosition : topLeftPosition,
+        size: designMode ? scaledSize : size,
+        type: 'select'
+    };
 }
 
-export const selectShape = ({ state }: Context) => {
-  const { events, currentDocument } = state;
-  Object.values(currentDocument.shapes).forEach((shape) =>
-    isPointInBox(events.pointer.position, shape)
-  );
+export const Select: FC<Props> = ({ key, name, position, size }) => {
+    console.log('rendering Select');
+
+    return (
+        <rect
+            data-cy={name}
+            height={size.height}
+            key={key}
+            strokeDasharray="5, 5"
+            style={inlineStyles}
+            width={size.width}
+            x={position.x}
+            y={position.y}
+        />
+    );
+};
+
+export const DesignSelect: FC = () => {
+    const pointer = usePointer();
+
+    const { key, name, position, size, type } = createSelectProps(pointer, true);
+
+    return <Select key={key} name={name} position={position} size={size} type={type} />;
 };
 
 export const SelectCommand: Command = {
-  id: 'select',
-  name: 'Select',
-  category: 'shapes',
-  description: 'Selects shapes and groups ',
-  icon: {
-    group: 'action',
-    name: 'pan_tool',
-    color: 'rgba(255,255,255)',
-    size: 24
-  },
-  regex: /(?<toolCode>select)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
-  shortcut: 's',
-  canExecute: (context: Context) => true,
-  execute: (context, args) => {
-    console.log('select executer');
-    React.createElement(Select, createSelectProps(context) as Props, null);
-  },
-  factory: (context: Context) => createSelectProps(context, true)
+    id: 'select',
+    name: 'Select',
+    category: 'shapes',
+    description: 'Selects shapes and groups ',
+    icon: {
+        group: 'action',
+        name: 'pan_tool',
+        color: 'rgba(255,255,255)',
+        size: 24
+    },
+    regex: /(?<toolCode>select)\((?<x1>[\d]+),(?<y1>[\d]+),(?<x2>[\d]+),(?<y2>[\d]+)\)/,
+    shortcut: 's',
+    canExecute: (context) => true,
+    execute: ({ actions, state: { events, currentDocument } }) => {
+        console.log('SelectCommand:execute');
+        const currentPointer = events.pointer.position;
+
+        Object.values(currentDocument.shapes).forEach((shape) => {
+            if (isPointInBox(currentPointer, shape)) {
+                shape.selected = true;
+            }
+        });
+    }
 };
 
 export const SelectTool: Tool = {
-  id: 'select',
-  name: 'Select',
-  description: 'Select a shape or group',
-  command: SelectCommand,
-  component: Select
+    id: 'select',
+    name: 'Select',
+    description: 'Select a shape or group',
+    command: SelectCommand,
+    designComponent: DesignSelect
 };
