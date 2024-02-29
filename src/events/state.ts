07260f8322b9d944e667bc6e0acdc21465a84298
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-29 23:27:34 +0100

    Update state.ts

--- src/events/state.ts
+++ src/events/state.ts
@@ -1,52 +1,51 @@
-import { string } from 'yargs';
 import {
-  bottomRightPosition,
-  center,
-  offset,
-  radius,
-  scaledCenter,
-  scaledPosition,
-  scaledOffset,
-  scaledPath,
-  scaledRadius,
-  scaledSize,
-  scaledBottomRightPosition,
-  scaledStartPosition,
-  scaledTopLeftPosition,
-  size,
-  topLeftPosition
+    bottomRight,
+    center,
+    offset,
+    radius,
+    scaledCenter,
+    scaledCurrent,
+    scaledOffset,
+    scaledPath,
+    scaledRadius,
+    scaledSize,
+    scaledBottomRight,
+    scaledStart,
+    scaledTopLeft,
+    size,
+    topLeft
 } from './computed/pointer';
 import { Events } from './types';
 
 export const state: Events = {
-  pointer: {
-    bottomRightPosition,
-    center,
-    dragging: false,
-    offset,
-    path: [],
-    position: { x: 0, y: 0 },
-    radius,
-    scaledCenter,
-    scaledOffset,
-    scaledPath,
-    scaledPosition,
-    scaledRadius,
-    scaledSize,
-    scaledBottomRightPosition,
-    scaledStartPosition,
-    scaledTopLeftPosition,
-    size,
-    startPosition: { x: 0, y: 0 },
-    topLeftPosition
-  },
-  keyboard: {
-    altKey: false,
-    ctrlKey: false,
-    key: '',
-    metaKey: false,
-    shiftKey: false,
-    text: '',
-    typing: false
-  }
+    pointer: {
+        bottomRight,
+        center,
+        dragging: false,
+        offset,
+        path: [],
+        current: { x: 0, y: 0 },
+        radius,
+        scaledCenter,
+        scaledOffset,
+        scaledPath,
+        scaledCurrent,
+        scaledRadius,
+        scaledSize,
+        scaledBottomRight,
+        scaledStart,
+        scaledTopLeft,
+        size,
+        start: { x: 0, y: 0 },
+        topLeft
+    },
+    keyboard: {
+        altKey: false,
+        ctrlKey: false,
+        key: '',
+        metaKey: false,
+        shiftKey: false,
+        text: '',
+        typing: false
+    }
 };
