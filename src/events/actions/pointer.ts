b22e8fd5f8ec9d27c89d9391efca45fa85341bcb
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-29 23:21:36 +0100

    Rename pointer.startPosition and pointer.position

--- src/events/actions/pointer.ts
+++ src/events/actions/pointer.ts
@@ -1,39 +1,39 @@
 import { ActionWithParam, Position } from '../../app/types';
 
-export const setStartPosition: ActionWithParam<Position> =  ({ state }, position) => {
-  state.events.pointer.startPosition = position;
+export const setStartPosition: ActionWithParam<Position> = ({ state }, position) => {
+    state.events.pointer.start = position;
 };
 
 export const setCurrentPosition: ActionWithParam<Position> = ({ state }, position) => {
-  state.events.pointer.position = position;
+    state.events.pointer.current = position;
 };
 
 export const updatePath: ActionWithParam<Position> = ({ state }, position) => {
-  state.events.pointer.path.push(position);
+    state.events.pointer.path.push(position);
 };
 
 export const startDragging: ActionWithParam<Position> = ({ state }, position) => {
-  const pointer = state.events.pointer;
-  pointer.dragging = true;
-  pointer.startPosition = position;
-  pointer.position = position;
-  pointer.path = [position];
+    const pointer = state.events.pointer;
+    pointer.dragging = true;
+    pointer.start = position;
+    pointer.current = position;
+    pointer.path = [position];
 };
 
-export const dragging: ActionWithParam<Position>  = ({ state }, position) => {
-  const pointer = state.events.pointer;
-  pointer.position = position;
-  pointer.path.push(position);
+export const dragging: ActionWithParam<Position> = ({ state }, position) => {
+    const pointer = state.events.pointer;
+    pointer.current = position;
+    pointer.path.push(position);
 };
 
-export const endDragging: ActionWithParam<Position>  = ({ state }) => {
-  state.events.pointer.dragging = false;
+export const endDragging: ActionWithParam<Position> = ({ state }) => {
+    state.events.pointer.dragging = false;
 };
 
 export const resetDragging: ActionWithParam<Position> = ({ state }) => {
-  const pointer = state.events.pointer;
-  pointer.dragging = false;
-  pointer.startPosition = { x: 0, y: 0 };
-  pointer.position = { x: 0, y: 0 };
-  pointer.path.length = 0;
+    const pointer = state.events.pointer;
+    pointer.dragging = false;
+    pointer.start = { x: 0, y: 0 };
+    pointer.current = { x: 0, y: 0 };
+    pointer.path.length = 0;
 };
