7915ac7b52a8d5f82b7510bcd8731bc8668d58d3
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-29 23:27:43 +0100

    Update types.ts

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,38 +1,38 @@
 import { Position, Size } from '../app/types';
 
 export interface Pointer {
-  bottomRightPosition: Position;
-  center: Position;
-  dragging: boolean;
-  offset: Position;
-  path: Position[];
-  position: Position;
-  radius: number;
-  scaledBottomRightPosition: Position;
-  scaledCenter: Position;
-  scaledOffset: Position;
-  scaledPath: Position[];
-  scaledPosition: Position;
-  scaledRadius: number;
-  scaledSize: Size;
-  scaledStartPosition: Position;
-  scaledTopLeftPosition: Position;
-  size: Size;
-  startPosition: Position;
-  topLeftPosition: Position;
+    bottomRight: Position;
+    center: Position;
+    current: Position;
+    dragging: boolean;
+    offset: Position;
+    path: Position[];
+    radius: number;
+    scaledBottomRight: Position;
+    scaledCenter: Position;
+    scaledCurrent: Position;
+    scaledOffset: Position;
+    scaledPath: Position[];
+    scaledRadius: number;
+    scaledSize: Size;
+    scaledStart: Position;
+    scaledTopLeft: Position;
+    size: Size;
+    start: Position;
+    topLeft: Position;
 }
 
 export interface Keyboard {
-  altKey: boolean;
-  ctrlKey: boolean;
-  key: string;
-  metaKey: boolean;
-  shiftKey: boolean;
-  text: string;
-  typing: boolean;
+    altKey: boolean;
+    ctrlKey: boolean;
+    key: string;
+    metaKey: boolean;
+    shiftKey: boolean;
+    text: string;
+    typing: boolean;
 }
 
 export interface Events {
-  pointer: Pointer;
-  keyboard: Keyboard;
+    keyboard: Keyboard;
+    pointer: Pointer;
 }
