19e1fb2a885ff2fba28deeaaca8a14ccd509baf5
Jakub Tasek <H506TYA@s-mxs.net>
2024-02-29 01:54:34 +0100

    Update index.tsx

--- src/tools/components/Line/index.tsx
+++ src/tools/components/Line/index.tsx
@@ -1,67 +1,95 @@
 import React, { FC } from 'react';
 
 import styles from '../../styles.css';
+import type { Command, Position } from 'src/app/types';
+import type { Pointer } from '../../../events/types';
 import type { Tool } from 'src/tools/types';
-import { Command } from 'src/app/types';
-
-import { Context } from '../../../app';
+import { newShapeName } from '../../../app/factories';
+import { usePointer } from '../../../app/hooks';
 
 /**
  * Draws a line from the start point to the end point
  **/
 
 interface Props {
-  name: string;
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
-  selected: boolean;
-  type: string;
+    end: Position;
+    key: string;
+    name: string;
+    selected: boolean;
+    start: Position;
+    type: string;
 }
 
-export const createLineProps = ({ state }: Context) => {
-  const { startPosition, position } = state.events.pointer;
+export const createLineProps = (
+    { startPosition, position }: Pointer,
+    designMode = false
+): Props => {
+    const name = designMode ? 'Line x' : newShapeName();
+    const key = name.toLowerCase();
 
-  return {
-    name: 'Line x',
-    x1: startPosition.x,
-    y1: startPosition.y,
-    x2: position.x,
-    y2: position.y,
-    selected: true,
-    type: 'line'
-  };
+    return {
+        end: position,
+        key,
+        name,
+        selected: true,
+        start: startPosition,
+        type: 'line'
+    };
 };
 
-export const Line: FC<Props> = ({ name, x1, y1, x2, y2, selected }) => {
-  const className = selected ? `${styles.shape} ${styles.selected}` : styles.shape;
+export const Line: FC<Props> = ({ key, name, start, end, selected }) => {
+    const className = selected ? `${styles.shape} ${styles.selected}` : styles.shape;
+    console.log('rendering Line');
 
-  return <line key="line" data-name={name} className={className} x1={x1} y1={y1} x2={x2} y2={y2} />;
+    return (
+        <line
+            className={className}
+            data-cy={name}
+            key={key}
+            x1={start.x}
+            x2={end.x}
+            y1={start.y}
+            y2={end.y}
+        />
+    );
+};
+
+export const DesignLine: FC = () => {
+    const pointer = usePointer();
+
+    const props = createLineProps(pointer, true);
+
+    return <Line {...props} />;
 };
 
 export const LineCommand: Command = {
-  id: 'line',
-  name: 'Line',
-  category: 'shapes',
-  description: 'Draw a line',
-  icon: {
-    group: 'action',
-    name: 'timeline',
-    color: 'rgb(95, 216, 240)',
-    size: 24
-  },
-  regex: /(?<toolCode>line)\((?<x1>\d+),(?<y1>\d+),(?<x2>\d+),(?<y2>\d+)\)/,
-  shortcut: 'ctrl+l',
-  canExecute: (context) => true,
-  execute: (context, args?) => React.createElement(Line, createLineProps(context), null),
-  factory: (context: Context) => createLineProps(context)
+    id: 'line',
+    name: 'Line',
+    category: 'shapes',
+    description: 'Draw a line',
+    icon: {
+        group: 'action',
+        name: 'timeline',
+        color: 'rgb(95, 216, 240)',
+        size: 24
+    },
+    regex: /(?<toolCode>line)\((?<x1>\d+),(?<y1>\d+),(?<x2>\d+),(?<y2>\d+)\)/,
+    shortcut: 'ctrl+l',
+    canExecute: (context) => true,
+    execute: ({ actions, state }) => {
+        console.log('LineCommand:execute');
+
+        const shape = createLineProps(state.events.pointer, true);
+
+        actions.addShape(shape);
+    }
 };
 
 export const LineTool: Tool = {
-  id: 'line',
-  name: 'Line',
-  description: 'Draw a line',
-  command: LineCommand,
-  component: Line
+    id: 'line',
+    name: 'Line',
+    description: 'Draw a line',
+    command: LineCommand,
+    component: Line,
+    designComponent: DesignLine
 };
