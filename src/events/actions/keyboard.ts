4c5be4011d30b79d13a7cc7ad66bc083ef548cff
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-07 22:46:33 +0100

    Update keyboard.ts

--- src/events/actions/keyboard.ts
+++ src/events/actions/keyboard.ts
@@ -1,69 +1,67 @@
 import { ActionWithParam, Action } from 'src/app/types';
 
 export const typing: ActionWithParam<string> = (
-  {
-    state: {
-      events: { keyboard }
-    }
-  },
-  text
+    {
+        state: {
+            events: { keyboard }
+        }
+    },
+    text
 ) => {
-  keyboard.typing = true;
-  keyboard.text = text;
+    keyboard.typing = true;
+    keyboard.text = text;
 };
 
 const EMPTY_STRING = '';
-export const startTyping: Action = (
-  {
+export const startTyping: Action = ({
     state: {
-      events: { keyboard }
+        events: { keyboard }
     }
-  }) => {
-  keyboard.text = EMPTY_STRING;
-  keyboard.typing = true;
+}) => {
+    keyboard.text = EMPTY_STRING;
+    keyboard.typing = true;
 };
 
-export const endTyping: Action = (
-  {
+export const endTyping: Action = ({
     state: {
-      events: { keyboard }
+        events: { keyboard }
     },
     actions
-  }) => {
-  actions.tools.executeToolCommands();
+}) => {
+    actions.tools.executeToolCommands();
 
-  keyboard.typing = false;
-  keyboard.text = EMPTY_STRING;
+    keyboard.typing = false;
+    keyboard.text = EMPTY_STRING;
 
-  actions.tools.resetTools();
+    actions.tools.resetTools();
 };
 
 export const keyDown: ActionWithParam<KeyboardEvent> = (
-  {
-    state: {
-      events: { keyboard }
-    }
-  },
-  event
+    {
+        state: {
+            events: { keyboard }
+        }
+    },
+    event
 ) => {
-  keyboard.altKey = event.altKey;
-  keyboard.ctrlKey = event.ctrlKey;
-  keyboard.key = event.key;
-  keyboard.metaKey = event.metaKey;
-  keyboard.shiftKey = event.shiftKey;
+    keyboard.altKey = event.altKey;
+    keyboard.ctrlKey = event.ctrlKey;
+    keyboard.key = event.key;
+    keyboard.metaKey = event.metaKey;
+    keyboard.shiftKey = event.shiftKey;
 };
 
 export const keyUp: ActionWithParam<KeyboardEvent> = (
-  {
-    state: {
-      events: { keyboard }
-    }
-  },
-  event
+    {
+        state: {
+            events: { keyboard }
+        }
+    },
+    event
 ) => {
-  keyboard.altKey = event.altKey;
-  keyboard.ctrlKey = event.ctrlKey;
-  keyboard.key = event.key;
-  keyboard.metaKey = event.metaKey;
-  keyboard.shiftKey = event.shiftKey;
+    keyboard.altKey = event.altKey;
+    keyboard.ctrlKey = event.ctrlKey;
+    keyboard.key = event.key;
+    keyboard.metaKey = event.metaKey;
+    keyboard.shiftKey = event.shiftKey;
 };
