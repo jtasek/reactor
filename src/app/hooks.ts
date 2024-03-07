8422a4539e871cb11cf8b691fa079fff901c5ac5
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-07 22:46:17 +0100

    Create useDebugMode and useLog hooks

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,104 +1,131 @@
-import { createActionsHook, createEffectsHook, createReactionHook, createStateHook } from 'overmind-react';
+import {
+    createActionsHook,
+    createEffectsHook,
+    createReactionHook,
+    createStateHook
+} from 'overmind-react';
 import { getCommand, getCommands } from './actions';
 import { Context } from '.';
+import { useCallback } from 'react';
 
 export const useActions = createActionsHook<Context>();
 export const useEffects = createEffectsHook<Context>();
 export const useReaction = createReactionHook<Context>();
 export const useAppState = createStateHook<Context>();
 
+export const useConfig = () => {
+    return useAppState((state) => state.config);
+};
+
+export const useDebugMode = () => {
+    return useConfig().debugMode;
+};
+
 export const useCommand = (commandId: string) => {
-  return getCommand(commandId);
+    return getCommand(commandId);
 };
 
 export const useCommands = () => {
-  return getCommands();
+    return getCommands();
 };
 
 export const useComponent = (id: string) => {
-  return useCurrentDocument()?.components[id];
+    return useCurrentDocument()?.components[id];
 };
 
 export const useComponents = () => {
-  return useCurrentDocument()?.components;
+    return useCurrentDocument()?.components;
 };
 
 export const useEvents = () => {
-  return useAppState((state) => state.events);
+    return useAppState((state) => state.events);
 };
 
 export const useKeyboard = () => {
-  return useAppState((state) => state.events.keyboard);
+    return useAppState((state) => state.events.keyboard);
 };
 
 export const usePointer = () => {
-  return useAppState((state) => state.events.pointer);
+    return useAppState((state) => state.events.pointer);
 };
 
 export const useTools = () => {
-  return useAppState((state) => state.tools);
+    return useAppState((state) => state.tools);
 };
 
 export const useControls = () => {
-  return useAppState((state) => state.ui);
+    return useAppState((state) => state.ui);
 };
 
 export const useCurrentDocument = () => {
-  return useAppState((state) => state.currentDocument);
+    return useAppState((state) => state.currentDocument);
 };
 
 export const useCurrentPage = () => {
-  return useAppState((state) => state.currentPage);
+    return useAppState((state) => state.currentPage);
 };
 
 export const useCamera = () => {
-  return useAppState((state) => state.currentDocument.camera);
+    return useAppState((state) => state.currentDocument.camera);
 };
 
 export const useDocument = (id: string) => {
-  return useAppState((state) => state.documents[id]);
+    return useAppState((state) => state.documents[id]);
 };
 
 export const useDocuments = () => {
-  return useAppState((state) => state.documents) ?? [];
+    return useAppState((state) => state.documents) ?? [];
 };
 
 export const useShape = (id: string) => {
-  return useCurrentDocument()?.shapes[id];
+    return useCurrentDocument()?.shapes[id];
 };
 
 export const useShapes = () => {
-  return useCurrentDocument()?.shapes ?? [];
+    return useCurrentDocument()?.shapes ?? [];
 };
 
 export const useRuler = (id: string) => {
-  return useCurrentDocument()?.rulers[id];
+    return useCurrentDocument()?.rulers[id];
 };
 
 export const useRulers = () => {
-  return useCurrentDocument()?.rulers ?? [];
+    return useCurrentDocument()?.rulers ?? [];
 };
 
 export const useGroup = (id: string) => {
-  return useCurrentDocument()?.groups[id];
+    return useCurrentDocument()?.groups[id];
 };
 
 export const useGroups = () => {
-  return useCurrentDocument()?.groups ?? [];
+    return useCurrentDocument()?.groups ?? [];
 };
 
 export const useLink = (id: string) => {
-  return useCurrentDocument()?.links[id];
+    return useCurrentDocument()?.links[id];
 };
 
 export const useLinks = () => {
-  return useCurrentDocument()?.links ?? [];
+    return useCurrentDocument()?.links ?? [];
 };
 
 export const useLayer = (id: string) => {
-  return useCurrentDocument()?.layers[id];
+    return useCurrentDocument()?.layers[id];
 };
 
 export const useLayers = () => {
-  return useCurrentDocument()?.layers ?? [];
+    return useCurrentDocument()?.layers ?? [];
+};
+
+export const useLog = () => {
+    const debugMode = useDebugMode();
+
+    return useCallback(
+        (message?: any, ...optionalParams: any[]) => {
+            if (debugMode) {
+                console.log(message, ...optionalParams);
+            }
+        },
+        [debugMode]
+    );
 };
