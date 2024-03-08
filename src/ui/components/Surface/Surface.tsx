4de75465a256a1b3bab0c3d9530689572b31505f
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-08 12:22:20 +0100

    Update Surface.tsx

--- src/ui/components/Surface/Surface.tsx
+++ src/ui/components/Surface/Surface.tsx
@@ -1,41 +1,12 @@
 import React, { FC, ReactNode } from 'react';
 
-import { usePointerDriver } from 'src/events/drivers/usePointerDriver';
-
 import styles from './styles.css';
 
 interface Props {
     children?: ReactNode;
 }
-export const Surface: FC<Props> = ({ children }) => {
-    const {
-        handleContextMenu,
-        handleMouseWheel,
-        handlePointerDown,
-        handlePointerMove,
-        handlePointerUp,
-        handleTouchEnd,
-        handleTouchMove,
-        handleTouchStart
-    } = usePointerDriver();
-
-    return (
-        <svg
-            className={styles.surface}
-            preserveAspectRatio="none"
-            // mouse
-            onContextMenu={handleContextMenu}
-            onPointerDown={handlePointerDown}
-            // onPointerEnter={handlePointerDown}
-            // onPointerLeave={handlePointerUp}
-            onPointerMove={handlePointerMove}
-            onPointerUp={handlePointerUp}
-            onTouchEnd={handleTouchEnd}
-            onTouchMove={handleTouchMove}
-            onTouchStart={handleTouchStart}
-            onWheel={handleMouseWheel}
-        >
-            {children}
-        </svg>
-    );
-};
+export const Surface: FC<Props> = ({ children }) => (
+    <svg className={styles.surface} preserveAspectRatio="none">
+        {children}
+    </svg>
+);
