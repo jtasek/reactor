ff2bfbce21e705196070f68306577e7ed6f3799d
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-14 22:22:10 +0100

    Update startup.ts

--- src/app/actions/startup.ts
+++ src/app/actions/startup.ts
@@ -1,56 +1,81 @@
 import { Command } from 'src/app/types';
 import { Overmind } from 'overmind';
-import { commands } from 'src/commands';
+import {
+    DeleteCommand,
+    CloneCommand,
+    MoveCommand,
+    GroupCommand,
+    UngroupCommand,
+    LayerCommand,
+    UnlayerCommand,
+    ZoomInCommand,
+    ZoomOutCommand,
+    ZoomResetCommand
+} from 'src/commands';
 import { Context } from '../index';
 
+const commands = new Map<string, Command>();
+
 export function registerCommand(command: Command) {
-  if (!commands[command.id]) {
-    commands[command.id] = command;
-  }
+    commands.set(command.id, command);
 }
 
 export function getCommands() {
-  return commands;
+    return commands;
 }
 
 export function getCommand(commandId: string) {
-  return commands[commandId];
+    return commands[commandId];
 }
 
 function registerCommands(state, instance) {
-  console.log('register commands');
+    console.log('register commands');
+
+    registerCommand(DeleteCommand);
+    registerCommand(CloneCommand);
+    registerCommand(MoveCommand);
+    registerCommand(GroupCommand);
+    registerCommand(UngroupCommand);
+    registerCommand(LayerCommand);
+    registerCommand(UnlayerCommand);
+    registerCommand(ZoomInCommand);
+    registerCommand(ZoomOutCommand);
+    registerCommand(ZoomResetCommand);
 }
 
 function loadLocalData(effects, state) {
-  const savedApp = effects.loadState('reactor');
-  if (savedApp) {
-    state.documents = savedApp.documents;
-  }
+    const savedApp = effects.loadState('reactor');
+    if (savedApp) {
+        state.documents = savedApp.documents;
+    }
 }
 
 function activateAutosave(instance, effects) {
-  console.log('Autosave is on');
+    console.log('Autosave is on');
 
-  instance.reaction(
-    (state) => state.currentDocument,
-    (document) => effects.saveState('reactor', document),
-    { nested: true }
-  );
+    instance.reaction(
+        (state) => state.currentDocument,
+        (document) => effects.saveState('reactor', document),
+        { nested: true }
+    );
 }
 
 function registerRoutes(effects, actions) {
-  effects.initializeRoutes({
-    '/': actions.showDesigner,
-    '/documents': actions.showDocuments
-  });
+    effects.initializeRoutes({
+        '/': actions.showDesigner,
+        '/documents': actions.showDocuments
+    });
 }
 
-// Do not rename, it's a fixed name
-export const onInitializeOvermind = ({ state, effects, actions}: Context, instance: Overmind<Context>) => {
-  registerCommands(state, instance);
-  registerRoutes(effects, actions);
-  loadLocalData(effects, state);
-  if (state.config.autoSave) {
-    activateAutosave(instance, effects);
-  }
+/**  Do not rename, it's a mandatory action name! */
+export const onInitializeOvermind = (
+    { state, effects, actions }: Context,
+    instance: Overmind<Context>
+) => {
+    registerCommands(state, instance);
+    registerRoutes(effects, actions);
+    loadLocalData(effects, state);
+    if (state.config.autoSave) {
+        activateAutosave(instance, effects);
+    }
 };
