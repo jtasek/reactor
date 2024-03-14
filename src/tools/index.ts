6ab549de6fbc9fd55d5ea1d0cc9734441caf1ddb
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-14 23:41:06 +0100

    Update index.ts

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,11 @@
-import { state } from './state';
-import * as actions from './actions';
+export { state } from './state';
+export * as actions from './actions';
 
-export { actions, state };
+export { CircleCommand } from './components/Circle';
+export { EllipseCommand } from './components/Ellipse';
+export { ImageCommand } from './components/Image';
+export { LineCommand } from './components/Line';
+export { PenCommand } from './components/Pen';
+export { RectCommand } from './components/Rect';
+export { SelectCommand } from './components/Select';
+export { TextCommand } from './components/Text';
