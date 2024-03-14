764af87f661e33542d6a48a8e8a6b763ea4509a4
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-14 22:22:36 +0100

    Update useKeyboardDriver.ts

--- src/events/drivers/useKeyboardDriver.ts
+++ src/events/drivers/useKeyboardDriver.ts
@@ -1,16 +1,22 @@
 import { useEffect } from 'react';
 import { useKeyboardAdapter } from './useKeyboardAdapter';
+import { useLog } from '../../app/hooks';
 
 export const useKeyboardDriver = () => {
     const { handleKeyDown, handleKeyUp } = useKeyboardAdapter();
+    const log = useLog();
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
         window.addEventListener('keyup', handleKeyUp);
 
+        log('Keyboard driver installed.');
+
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
             window.removeEventListener('keyup', handleKeyUp);
+
+            log('Keyboard driver removed.');
         };
     }, []);
 };
