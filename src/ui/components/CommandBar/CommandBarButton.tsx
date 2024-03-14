53aad3c8df44767855b19558c82c879caf67733a
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-14 23:41:21 +0100

    Update CommandBarButton.tsx

--- src/ui/components/CommandBar/CommandBarButton.tsx
+++ src/ui/components/CommandBar/CommandBarButton.tsx
@@ -5,26 +5,37 @@ import { Command } from 'src/app/types';
 import { useActions } from 'src/app/hooks';
 
 export interface Props {
-  command: Command;
+    active: boolean;
+    command: Command;
+    onClick: () => void;
 }
 
-export const CommandBarButton: FC<Props> = ({ command }) => {
-  const { canExecuteCommand, executeCommand } = useActions();
-  
-  return (
-    <li className={styles.commandBarButton}>
-      <a
-        href="#"
-        onClick={(e) => {
-          e.preventDefault();
-          executeCommand(command.execute)
-        }}
-        title={command.description}
-      >
-        <Icon icon={command.icon} />
-        {canExecuteCommand(command.canExecute) ? '' : '*'}
-        {command.name}
-      </a>
-    </li>
-  );
+export const CommandBarButton: FC<Props> = ({ active, command, onClick }) => {
+    const { canExecuteCommand, executeCommand } = useActions();
+    const enabled = canExecuteCommand(command.canExecute);
+
+    return (
+        <li className={styles.commandBarButton} aria-disabled={!enabled}>
+            {enabled && (
+                <a
+                    href="#"
+                    onClick={(event) => {
+                        event.preventDefault();
+                        onClick();
+                        // executeCommand(command.execute);
+                    }}
+                    title={command.description}
+                >
+                    <Icon icon={command.icon} />
+                    {command.name}
+                </a>
+            )}
+            {!enabled && (
+                <div>
+                    <Icon icon={command.icon} />
+                    {command.name}
+                </div>
+            )}
+        </li>
+    );
 };
