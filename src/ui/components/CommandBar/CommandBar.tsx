a8bd07f68c6e62d8cc92bb46212227585b9f14dc
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-14 23:41:16 +0100

    Update CommandBar.tsx

--- src/ui/components/CommandBar/CommandBar.tsx
+++ src/ui/components/CommandBar/CommandBar.tsx
@@ -1,50 +1,46 @@
 import React, { FC } from 'react';
 
 import { useCommands } from 'src/app/hooks';
-import { Command } from 'src/app/types';
+import type { Command } from 'src/app/types';
 
 import { CommandBarDelimiter } from './CommandBarDelimiter';
 import { CommandBarGroup } from './CommandBarGroup';
 import styles from './styles.css';
 
-function groupCommands(commands): Record<string, Command[]> {
-  if (commands?.length === 0) {
-    return {};
-  }
-
-  return commands.reduce((result, command) => {
-    if (result[command.category]) {
-      result[command.category].push(command);
-    } else {
-      result[command.category] = [command];
-    }
-    return result;
-  }, {});
+function groupCommands(commands: Record<string, Command>) {
+    return Object.values(commands).reduce((result, command) => {
+        if (result[command.category]) {
+            result[command.category].push(command);
+        } else {
+            result[command.category] = [command];
+        }
+        return result;
+    }, []);
 }
 
 export const CommandBar: FC = () => {
-  const commands = useCommands();
+    const commands = useCommands();
 
-  if (commands?.length === 0) {
-    return null;
-  }
+    if (Object.keys(commands).length === 0) {
+        return null;
+    }
 
-  const groups: [string, Command[]][] = Object.entries(groupCommands(commands));
-  const last = groups.pop();
+    const groups: [string, Command[]][] = Object.entries(groupCommands(commands));
+    const last = groups.pop();
 
-  if (!last || !groups) {
-    return null;
-  }
+    if (!last || !groups) {
+        return null;
+    }
 
-  return (
-    <ul className={styles.commandBar}>
-      {groups.map(([key, value]) => (
-        <>
-          <CommandBarGroup key={`item-${key}`} commands={value} />
-          <CommandBarDelimiter key={`delimiter-${key}`} />
-        </>
-      ))}
-      <CommandBarGroup commands={last[1]} />
-    </ul>
-  );
+    return (
+        <ul className={styles.commandBar}>
+            {groups.map(([key, value]) => (
+                <>
+                    <CommandBarGroup key={`item-${key}`} commands={value} />
+                    <CommandBarDelimiter key={`delimiter-${key}`} />
+                </>
+            ))}
+            <CommandBarGroup commands={last[1]} />
+        </ul>
+    );
 };
