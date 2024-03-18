63494ebf749e0047664fe905ca9cce3eb29d02ff
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-18 21:26:22 +0100

    Update index.tsx

--- src/ui/components/ToolBar/index.tsx
+++ src/ui/components/ToolBar/index.tsx
@@ -1,15 +1,14 @@
 import React, { FC } from 'react';
 
-import { useActions, useControls } from 'src/app/hooks';
+import { useControls } from 'src/app/hooks';
 import { ToolBar } from './ToolBar';
 
 export const ToolBarContainer: FC = () => {
-  const { toolBar } = useControls();
-  const actions = useActions();
+    const { toolBar } = useControls();
 
-  if (!toolBar.visible) {
-    return null;
-  }
+    if (!toolBar.visible) {
+        return null;
+    }
 
-  return <ToolBar onClick={actions.tools.activateTool} />;
+    return <ToolBar />;
 };
