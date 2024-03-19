97d64c4aa4a8d5ea45ffdfb3ddca3abac653ffd1
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-19 09:04:18 +0100

    Update commands.ts

--- src/app/actions/commands.ts
+++ src/app/actions/commands.ts
@@ -3,10 +3,9 @@ import { Action, ActionGuard } from '../types';
 import { Context } from '../index';
 
 export const canExecuteCommand = (context: Context, actionGuard: ActionGuard) => {
-  return actionGuard(context);
+    return actionGuard(context);
 };
 
-
 export const executeCommand = (context: Context, action: Action) => {
-  action(context);
+    action(context);
 };
