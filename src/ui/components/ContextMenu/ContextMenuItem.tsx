2aac35f119989f43e92195b3a919f297a26f27ff
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-19 09:05:28 +0100

    Update ContextMenuItem.tsx

--- src/ui/components/ContextMenu/ContextMenuItem.tsx
+++ src/ui/components/ContextMenu/ContextMenuItem.tsx
@@ -4,23 +4,23 @@ import styles from './styles.css';
 import { Tool } from 'src/tools/types';
 
 export interface Props {
-  tool: Tool;
-  inlineStyles: any;
-  onClick: (tool: string) => void;
+    tool: Tool;
+    inlineStyles: any;
+    onClick: (tool: string) => void;
 }
 
 export const ContextMenuItem: FC<Props> = ({ tool, inlineStyles, onClick }) => (
-  <li className={styles.contextMenuButton} style={inlineStyles}>
-    <a
-      href="#"
-      onClick={(e) => {
-        console.log('ContextMenuItem onClick');
-        e.preventDefault();
-        onClick(tool.id);
-      }}
-      title={tool.description}
-    >
-      <Icon icon={tool.command.icon} />
-    </a>
-  </li>
+    <li className={styles.contextMenuButton} style={inlineStyles}>
+        <a
+            href="#"
+            onClick={(e) => {
+                console.log('ContextMenuItem onClick');
+                e.preventDefault();
+                onClick(tool.id);
+            }}
+            title={tool.description}
+        >
+            <Icon icon={tool.icon} />
+        </a>
+    </li>
 );
