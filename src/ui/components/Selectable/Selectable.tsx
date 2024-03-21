e5abbf43a217ecaa1a6f7d50ffed41898ae92ac6
Jakub Tasek <H506TYA@s-mxs.net>
2024-03-21 09:18:35 +0100

    Update Selectable.tsx

--- src/ui/components/Selectable/Selectable.tsx
+++ src/ui/components/Selectable/Selectable.tsx
@@ -1,75 +1,39 @@
 import React, { FC, ReactNode } from 'react';
 
+import styles from './styles.css';
+import { Shape } from 'src/app/types';
+import { getBoundingBox } from '../../../app/utils';
 import { useActions } from 'src/app/hooks';
-import { Box, Shape } from 'src/app/types';
-import { getBoundingBox } from 'src/app/utils';
-import { Handle } from '../Handle';
 
 export interface Props {
-  shape: Shape
-  children?: ReactNode;
+    shape: Shape;
+    children?: ReactNode;
 }
 
-export const Resizable: FC<Props> = ({ shape, children }) => {
-  const box = getBoundingBox(shape);
+const SELECTABLE_OFFSET = 0;
 
-  if (!box?.topLeft || !box.bottomRight) {
-    return children;
-  }
+export const Selectable: FC<Props> = ({ shape }) => {
+    const { toggleShapeSelected } = useActions();
 
-  const {
-    topLeft,
-    topRight,
-    bottomLeft,
-    bottomRight,
-    middleLeft,
-    middleRight,
-    middleTop,
-    middleBottom
-  } = calcBoundingPoints(box);
+    if (!shape.selected) {
+        return null;
+    }
 
-  return (
-    <>
-      {children}
-      <Handle key="topLeft" position={topLeft} type="topLeft" shapeId={shape.id} />
-      <Handle key="topRight" position={topRight} type="topRight" shapeId={shape.id} />
-      <Handle key="bottomLeft" position={bottomLeft} type="bottomLeft" shapeId={shape.id} />
-      <Handle key="bottomRight" position={bottomRight} type="bottomRight" shapeId={shape.id} />
-      <Handle key="middleLeft" position={middleLeft} type="middleLeft" shapeId={shape.id} />
-      <Handle key="middleRight" position={middleRight} type="middleRight" shapeId={shape.id} />
-      <Handle key="middleTop" position={middleTop} type="middleTop" shapeId={shape.id} />
-      <Handle key="middleBottom" position={middleBottom} type="middleBottom" shapeId={shape.id} />
-    </>
-  );
+    const box = getBoundingBox(shape);
+
+    const x = box.topLeft.x - SELECTABLE_OFFSET;
+    const y = box.topLeft.y - SELECTABLE_OFFSET;
+    const width = box.width + SELECTABLE_OFFSET * 2;
+    const height = box.height + SELECTABLE_OFFSET * 2;
+
+    return (
+        <rect
+            x={x}
+            y={y}
+            height={height}
+            width={width}
+            className={styles.selectable}
+            onClick={() => toggleShapeSelected(shape.id)}
+        />
+    );
 };
-
-export const Selectable: FC<Props> = ({ shape, children }) => {
-  const { toggleShapeSelected } = useActions();
-
-  return <g onClick={() => toggleShapeSelected(shape.id)}>{children}</g>;
-};
-
-function calcBoundingPoints(box: Box) {
-  const topLeft = box.topLeft;
-  const topRight = { x: box.bottomRight.x, y: box.topLeft.y };
-  const bottomLeft = { x: box.topLeft.x, y: box.bottomRight.y };
-  const bottomRight = box.bottomRight;
-  const height = bottomLeft.y - topLeft.y;
-  const width = bottomRight.x - topLeft.x;
-
-  const middleLeft = { x: topLeft.x, y: topLeft.y + height / 2 };
-  const middleRight = { x: bottomRight.x, y: topRight.y + height / 2 };
-  const middleTop = { x: topLeft.x + width / 2, y: topLeft.y };
-  const middleBottom = { x: bottomLeft.x + width / 2, y: bottomRight.y };
-
-  return {
-    topLeft,
-    topRight,
-    bottomLeft,
-    bottomRight,
-    middleLeft,
-    middleRight,
-    middleTop,
-    middleBottom
-  };
-}
