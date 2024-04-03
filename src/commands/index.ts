7be05971307414cfb875019a10c0dbfde55bff14
Jakub Tasek <H506TYA@s-mxs.net>
2024-04-03 22:06:29 +0200

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,7 @@
-export { DeleteCommand } from './delete';
 export { CloneCommand } from './clone';
-export { MoveCommand } from './move';
+export { DeleteCommand } from './delete';
 export { GroupCommand, UngroupCommand } from './group';
 export { LayerCommand, UnlayerCommand } from './layer';
+export { MoveCommand } from './move';
+export { PanCommand } from './pan';
 export { ZoomInCommand, ZoomOutCommand, ZoomResetCommand } from './zoom';
