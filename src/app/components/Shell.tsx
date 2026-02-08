ac468a824a9187f1c93055708c8f504a24adc676
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-08 21:49:57 +0100

    Update Shell.tsx

--- src/app/components/Shell.tsx
+++ src/app/components/Shell.tsx
@@ -1,15 +1,12 @@
 import React, { FC } from 'react';
+import { Designer, Documents } from '../../pages';
 import { useCurrentPage } from '../hooks';
 import { useKeyboardDriver } from 'src/events/drivers/useKeyboardDriver';
-import { usePointerDriver } from 'src/events/drivers/usePointerDriver';
 import { usePreventNativePinchZoom } from './usePreventNativePinchZoom';
-
-import { Designer, Documents } from '../../pages';
 
 export const Shell: FC = () => {
     usePreventNativePinchZoom();
     useKeyboardDriver();
-    usePointerDriver();
 
     const currentPage = useCurrentPage();
 
