54c9ea4881e27c41e4e4a0bf31b281a22eada2c2
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-08 21:53:50 +0100

    Update Surface.tsx

--- src/ui/components/Surface/Surface.tsx
+++ src/ui/components/Surface/Surface.tsx
@@ -1,12 +1,38 @@
 import React, { FC, ReactNode } from 'react';
+import { usePointerAdapter } from '../../../events/drivers/usePointerAdapter';
 
 import styles from './styles.css';
 
 interface Props {
     children?: ReactNode;
 }
-export const Surface: FC<Props> = ({ children }) => (
-    <svg className={styles.surface} preserveAspectRatio="none">
-        {children}
-    </svg>
-);
+export const Surface: FC<Props> = ({ children }) => {
+    const svgRef = React.useRef<SVGSVGElement>(null);
+
+    const {
+        handleContextMenu,
+        handleMouseWheel,
+        handlePointerDown,
+        handlePointerMove,
+        handlePointerUp
+    } = usePointerAdapter(svgRef);
+
+    return (
+        <svg
+            className={styles.surface}
+            preserveAspectRatio="none"
+            ref={svgRef}
+            onContextMenu={handleContextMenu}
+            onPointerDown={handlePointerDown}
+            onPointerMove={handlePointerMove}
+            onPointerUp={handlePointerUp}
+            onWheel={handleMouseWheel}
+            style={{
+                touchAction: 'none', // 🔑 prevents native pinch zoom / scrolling
+                userSelect: 'none'
+            }}
+        >
+            {children}
+        </svg>
+    );
+};
