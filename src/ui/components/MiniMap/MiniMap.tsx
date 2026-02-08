9b2c344418bc7e1f9647ba11b6218196bb3e5e4e
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-08 22:44:03 +0100

    Update MiniMap.tsx

--- src/ui/components/MiniMap/MiniMap.tsx
+++ src/ui/components/MiniMap/MiniMap.tsx
@@ -4,12 +4,12 @@ import styles from './styles.css';
 import { Size } from 'src/app/types';
 
 export interface Props {
-  size: Size;
-  children?: ReactNode;
+    size: Size;
+    children?: ReactNode;
 }
 
 export const MiniMap: FC<Props> = ({ size, children }) => (
-  <svg className={styles.minimap} width={size.width} height={size.height} viewBox="0 0 1000 1000">
-    {children}
-  </svg>
+    <svg className={styles.minimap} width={size.width} height={size.height} viewBox="0 0 1000 1000">
+        {children}
+    </svg>
 );
