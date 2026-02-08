036afe1c5016a6f9452c57c34e9c9ec8ce8b3836
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-08 22:43:58 +0100

    Update Camera.tsx

--- src/ui/components/Surface/Camera.tsx
+++ src/ui/components/Surface/Camera.tsx
@@ -4,18 +4,19 @@ import { useCamera } from 'src/app/hooks';
 import styles from './styles.css';
 
 interface Props {
-  children?: ReactNode;
+    children?: ReactNode;
 }
 
 export const Camera: FC<Props> = ({ children }) => {
-  const { position, scale } = useCamera();
+    const { position, scale } = useCamera();
 
-  return (
-    <g
-      className={styles.camera}
-      transform={`translate(${position.x},${position.y}) scale(${scale})`}
-    >
-      {children}
-    </g>
-  );
+    return (
+        <g
+            id="camera"
+            className={styles.camera}
+            transform={`translate(${position.x},${position.y}) scale(${scale})`}
+        >
+            {children}
+        </g>
+    );
 };
