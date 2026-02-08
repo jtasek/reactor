500e15d2a9171d01715c644b39977ed8d4e3bd25
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-08 21:52:17 +0100

    Update usePreventNativePinchZoom.tsx

--- src/app/components/usePreventNativePinchZoom.tsx
+++ src/app/components/usePreventNativePinchZoom.tsx
@@ -1,18 +1,18 @@
 import { useEffect } from 'react';
 
 export const usePreventNativePinchZoom = () => {
-  const handleMouseWheel = (event) => {
-    if (event.ctrlKey) {
-      event.preventDefault();
-    }
-    console.log(`wheel event`);
-  };
+    const handleMouseWheel = (event: WheelEvent) => {
+        if (event.ctrlKey) {
+            event.preventDefault();
+        }
+        console.log('Prevent native pinch zoom.');
+    };
 
-  useEffect(() => {
-    document.addEventListener('wheel', handleMouseWheel, {
-      passive: false
-    });
+    useEffect(() => {
+        document.addEventListener('wheel', handleMouseWheel, {
+            passive: false
+        });
 
-    return () => document.removeEventListener('wheel', handleMouseWheel);
-  }, []);
+        return () => document.removeEventListener('wheel', handleMouseWheel);
+    }, []);
 };
