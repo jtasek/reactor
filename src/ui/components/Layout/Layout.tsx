84c57ea020b1b0ad9268d2a49c4c6142a9a32d50
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-12 09:43:30 +0100

    Update Layout.tsx

--- src/ui/components/Layout/Layout.tsx
+++ src/ui/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
 import React, { FC, PropsWithChildren } from 'react';
 import styles from './styles.css';
 
-export const Layout: FC<PropsWithChildren> = ({ children }) => <div>picaaa{children}</div>;
+export const Layout: FC<PropsWithChildren> = ({ children }) => <div>{children}</div>;
