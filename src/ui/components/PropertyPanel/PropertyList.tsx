d55de01bab705789f5c3669dce65b57907bfd841
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-12 09:44:02 +0100

    Update PropertyList.tsx

--- src/ui/components/PropertyPanel/PropertyList.tsx
+++ src/ui/components/PropertyPanel/PropertyList.tsx
@@ -4,23 +4,23 @@ import { PropertyGroupItem } from './PropertyGroupItem';
 import { PropertyPanelItem } from './PropertyPanelItem';
 
 interface Props {
-  items: [string, any][];
+    items: [string, any][];
 }
 
 export const PropertyList: FC<Props> = ({ items }) => {
-  return (
-    <>
-      {items.map(([name, value]) => {
-        if (typeof value === 'object') {
-          return (
-            <PropertyGroupItem key={`${name}-group`} name={name}>
-              <PropertyList key={name} items={Object.entries(value)} />
-            </PropertyGroupItem>
-          );
-        }
+    return (
+        <>
+            {items.map(([name, value]) => {
+                if (typeof value === 'object') {
+                    return (
+                        <PropertyGroupItem key={`${name}-group`} name={name}>
+                            <PropertyList key={name} items={Object.entries(value)} />
+                        </PropertyGroupItem>
+                    );
+                }
 
-        return <PropertyPanelItem key={name} name={name} value={getPropValue(value)} />;
-      })}
-    </>
-  );
+                return <PropertyPanelItem key={name} name={name} value={getPropValue(value)} />;
+            })}
+        </>
+    );
 };
