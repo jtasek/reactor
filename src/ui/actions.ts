20c1e287ae835b7d48e51cb7fe2e5083fe764a6b
Jakub Tasek <H506TYA@s-mxs.net>
2026-02-12 09:43:09 +0100

    Update actions.ts

--- src/ui/actions.ts
+++ src/ui/actions.ts
@@ -1,37 +1,38 @@
-import { ActionWithParam, Action } from 'src/app/types';
+import { ActionWithParam, Action, Point } from 'src/app/types';
 
-export const displayContextMenu: Action = ({ state }) => {
-  state.ui.contextMenu.visible = true;
+export const displayContextMenu: ActionWithParam<Point> = ({ state }, position) => {
+    state.ui.contextMenu.position = position;
+    state.ui.contextMenu.visible = true;
 };
 
 export const hideContextMenu: Action = ({ state }) => {
-  state.ui.contextMenu.visible = false;
+    state.ui.contextMenu.visible = false;
 };
 
 export const showControl: ActionWithParam<string> = ({ state }, controlId: string) => {
-  state.ui[controlId].visible = true;
+    state.ui[controlId].visible = true;
 };
 
 export const hideControl: ActionWithParam<string> = ({ state }, controlId: string) => {
-  state.ui[controlId].visible = false;
+    state.ui[controlId].visible = false;
 };
 
 export const toggleControlVisibility: ActionWithParam<string> = ({ state }, controlId: string) => {
-  state.ui[controlId].visible = !state.ui[controlId].visible;
+    state.ui[controlId].visible = !state.ui[controlId].visible;
 };
 
-export const distractionFreeMode: Action= ({ state }) => {
-  state.ui.commandLine.visible = false;
-  state.ui.contextMenu.visible = false;
-  state.ui.controlPanel.visible = false;
-  state.ui.groupPanel.visible = false;
-  state.ui.layerPanel.visible = false;
-  state.ui.menuBar.visible = false;
-  state.ui.miniMap.visible = false;
-  state.ui.navBar.visible = false;
-  state.ui.propertyPanel.visible = false;
-  state.ui.sideBar.visible = false;
-  state.ui.statusBar.visible = false;
-  state.ui.surface.visible = false;
-  state.ui.toolBar.visible = false;
+export const distractionFreeMode: Action = ({ state }) => {
+    state.ui.commandLine.visible = false;
+    state.ui.contextMenu.visible = false;
+    state.ui.controlPanel.visible = false;
+    state.ui.groupPanel.visible = false;
+    state.ui.layerPanel.visible = false;
+    state.ui.menuBar.visible = false;
+    state.ui.miniMap.visible = false;
+    state.ui.navBar.visible = false;
+    state.ui.propertyPanel.visible = false;
+    state.ui.sideBar.visible = false;
+    state.ui.statusBar.visible = false;
+    state.ui.surface.visible = false;
+    state.ui.toolBar.visible = false;
 };
